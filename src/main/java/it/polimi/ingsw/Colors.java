6b639402d492387224a9912188336a965203de58
Amrit <amrit6422@gmail.com>
2022-03-20 16:59:32 +0100

    Defined Enum

--- src/main/java/it/polimi/ingsw/Colors.java
+++ src/main/java/it/polimi/ingsw/Colors.java
@@ -1,15 +1,34 @@
 package it.polimi.ingsw;
 
+import java.io.Serializable;
+
 /**
  * @author Amrit
  */
-public enum Colors {
-    YELLOW,
-    BLUE,
-    GREEN,
-    RED,
-    PINK,
-    BLACK,
-    GREY,
-    WHITE
+public enum Colors implements Serializable {
+    YELLOW(0),
+    BLUE(1),
+    GREEN(2),
+    RED(3),
+    PINK(4),
+    BLACK(5),
+    GREY(6),
+    WHITE(7);
+
+    private final int index;
+
+    Colors(int index){
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public static Colors getColor(int index) throws IncorrectArgumentException {
+        for(Colors color: values()){
+            if(color.getIndex() == index) return color;
+        }
+        throw new IncorrectArgumentException("Index not associated with any color");
+    }
 }
