777fb4792ac3ca3e636b9650020174667a3bc898
Amrit <amrit6422@gmail.com>
2022-03-20 16:59:21 +0100

    Defined addStudents, removeStudents, drawStudents

--- src/main/java/it/polimi/ingsw/Bag.java
+++ src/main/java/it/polimi/ingsw/Bag.java
@@ -1,18 +1,76 @@
 package it.polimi.ingsw;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Amrit
  */
 public class Bag {
-    private ArrayList<StudentDisc> students;
+    private HashMap<StudentDisc, Integer> students;
+    private final int studentType = 5;
 
-    public void addStudents(ArrayList<StudentDisc> students){
+    public Bag(HashMap<StudentDisc, Integer> students){
+        this.students = students;
+    }
+
+    public void addStudents(HashMap<StudentDisc, Integer> students) throws IncorrectArgumentException {
+        HashMap<StudentDisc, Integer> studentsDiscs = this.getStudents();
+
+        for(Map.Entry<StudentDisc, Integer> set : students.entrySet()) {
+            if (set.getValue() >= 0){
+                if(studentsDiscs.containsKey(set.getKey())){
+                    studentsDiscs.put(set.getKey(), set.getValue() + studentsDiscs.get(set.getKey()));
+                } else {
+                    studentsDiscs.put(set.getKey(), set.getValue());
+                }
+            } else {
+                throw new IncorrectArgumentException("HashMap is not correct");
+            }
+
+        }
+        this.setStudents(studentsDiscs);
+    }
+
+    public HashMap<StudentDisc, Integer> drawStudents() throws IncorrectArgumentException {
+        int n = 3;
+        int type, quantity;
+        HashMap<StudentDisc, Integer> studentsDrawn = new HashMap<>();
+
+        for (int i = 0; i < n;){
+            type = (int)Math.floor(Math.random()*(studentType + 1));
+            quantity = (int)Math.floor(Math.random()*(n-i)+1);
+            studentsDrawn.put(new StudentDisc(Colors.getColor(type)), quantity);
+            i += quantity;
+        }
+
+        this.removeStudents(studentsDrawn);
+        return studentsDrawn;
 
     }
 
-    public ArrayList<StudentDisc> drawStudents() {
-        return new ArrayList<>();
+    private void removeStudents(HashMap<StudentDisc, Integer> studentsToRemove) throws IncorrectArgumentException {
+        HashMap<StudentDisc, Integer> studentsDiscs = this.getStudents();
+
+        for(Map.Entry<StudentDisc, Integer> set : studentsToRemove.entrySet()) {
+            if (set.getValue() >= 0){
+                if(studentsDiscs.containsKey(set.getKey())){
+                    studentsDiscs.put(set.getKey(), studentsDiscs.get(set.getKey()) - set.getValue());
+                } else {
+                    throw new IncorrectArgumentException("HashMap is not correct");
+                }
+            } else {
+                throw new IncorrectArgumentException("HashMap is not correct");
+            }
+        }
+        this.setStudents(studentsDiscs);
+    }
+
+    public HashMap<StudentDisc, Integer> getStudents() {
+        return students;
+    }
+
+    public void setStudents(HashMap<StudentDisc, Integer> students) {
+        this.students = students;
     }
 }
