45a998dec4e65e5573f85f842aeb50e40642ef4c
Amrit <amrit6422@gmail.com>
2022-03-22 13:40:14 +0100

    moveStudents method implemented

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -3,14 +3,16 @@ package it.polimi.ingsw.model;
 import it.polimi.ingsw.AssistantCard;
 import it.polimi.ingsw.AssistantCardDeck;
 import it.polimi.ingsw.model.enumerations.Colors;
+import it.polimi.ingsw.model.exceptions.IncorrectArgumentException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Amrit
  */
-public class Player implements Comparable{
+public class Player implements Comparable {
     private String nickname;
     private SchoolBoard schoolBoard;
     private Colors towerColors;
@@ -18,28 +20,38 @@ public class Player implements Comparable{
     private AssistantCard playedCard;
     private int characterCard;
 
-    public Player(String nickname, Colors towerColors, int numberOfPlayers){
+    public Player(String nickname, Colors towerColors, int numberOfPlayers) {
         this.nickname = nickname;
         this.schoolBoard = new SchoolBoard(numberOfPlayers);
         this.towerColors = towerColors;
 
     }
 
-    public void playAssistantCard(int index){
+    public void playAssistantCard(int index) {
+        playedCard = assistantCardDeck.getAssistantCard(index);
         assistantCardDeck.getAssistantCard(index).setHasPlayed(true);
-
     }
 
-    public boolean moveMotherNature(int distance){
+    public boolean moveMotherNature(int distance) {
         return true;
     }
 
-    public void moveStudents(HashMap<StudentDisc, Integer> students, ArrayList<Integer> destination){
-
-    }
-
-    public void insertStudentsFromCloud(HashMap<StudentDisc, Integer> students){
-
+    public void moveStudents(HashMap<StudentDisc, Integer> students, ArrayList<Integer> destinations) throws IncorrectArgumentException {
+        if (schoolBoard.hasEnoughStudents(students)) {
+            if (students.size() == destinations.size()) {
+                for (Integer destination : destinations) {
+                    if (destination == 0) {
+                        schoolBoard.moveStudents(students);
+                    } else {
+                        schoolBoard.removeStudents(students);
+                    }
+                }
+            } else {
+                throw new IncorrectArgumentException();
+            }
+        } else {
+            throw new IncorrectArgumentException();
+        }
     }
 
     @Override
