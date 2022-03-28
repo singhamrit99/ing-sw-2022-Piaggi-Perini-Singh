69de18692f0923b911629158357b2e7a9ebb518c
Tino <tinopiaggi@gmail.com>
2022-03-28 23:11:01 +0200

    Updates on Game methods

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.enumerations.Colors;
 import it.polimi.ingsw.model.enumerations.State;
 import it.polimi.ingsw.model.exceptions.IncorrectArgumentException;
 import it.polimi.ingsw.model.exceptions.IncorrectPlayerException;
@@ -8,10 +9,8 @@ import it.polimi.ingsw.model.exceptions.MotherNatureLostException;
 import it.polimi.ingsw.model.tiles.CloudTile;
 import it.polimi.ingsw.model.tiles.IslandTile;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.PriorityQueue;
+import java.awt.*;
+import java.util.*;
 
 public class Game {
     private int expertMode = 0;
@@ -30,6 +29,7 @@ public class Game {
     private int counter;
     private boolean playerDrawnOut;
     private ListIterator<Player> playerIterator;
+    private Player winner;
 
     public void pickCharacter(Player player) {
 
@@ -64,6 +64,8 @@ public class Game {
         playerDrawnOut = false;
         state = State.PLANNINGPHASE;
 
+        //initialization PriorityQueue<Player>
+        orderPlayers = new PriorityQueue<>(numOfPlayer);
     }
 
     public void drawFromBag(Player playerCaller) throws IncorrectArgumentException {
@@ -79,13 +81,15 @@ public class Game {
         if (state == State.PLANNINGPHASE) {
             if (player.equals(currentPlayer) && playerDrawnOut) {  //playerDrawnOut = player has drawn from bag
                 currentPlayer.playAssistantCard(indexCard);
-                nextPlayer(currentPlayer);
+
             } else {
                 throw new IncorrectPlayerException();
             }
         } else {
             throw new IncorrectStateException();
         }
+        orderPlayers.add(currentPlayer);
+        nextPlayer(currentPlayer);
     }
 
     private void nextPlayer(Player callerPlayer) throws IncorrectPlayerException, IncorrectArgumentException, IncorrectStateException {
@@ -143,9 +147,41 @@ public class Game {
         }
     }
 
+    //0 dining room , 1 to island tile
+    public void moveStudents(HashMap<StudentDisc, Integer> students, ArrayList<Integer> destinations, ArrayList<String> islandDestinations) throws IncorrectArgumentException {
+        int i = 0;
+        HashMap<StudentDisc, Integer> studentsToMoveToIsland = new HashMap<>();
+        if (students.size() == destinations.size()) {
+            for (Map.Entry<StudentDisc, Integer> set : students.entrySet()) {
+                if (destinations.get(i) == 1) {
+                    studentsToMoveToIsland.put(set.getKey(), set.getValue());
 
-    public void moveStudents() {
-        //currentPlayer.moveStudents();
+                } else {
+                    throw new IncorrectArgumentException();
+
+                }
+                i++;
+            }
+        } else {
+            throw new IncorrectArgumentException();
+        }
+
+        currentPlayer.moveStudents(students,destinations);
+        /*
+        if (studentsToMoveToIsland.size() != 0 && studentsToMoveToIsland.size() == islandDestinations.size()){
+            boolean found = false;
+            for (String dest : islandDestinations){
+                for(IslandTile island : islands ){
+                    if(island.getName().equals){
+                        found = true;
+                        //island.addStudents(studentsToMoveToIsland;
+                        break;
+                    }
+                }
+            }
+        }
+        */
+
     }
 
 
@@ -165,9 +201,18 @@ public class Game {
         }
     }
 
-    public void checkAndPlaceProfessor() {
-        //ask to each player how many students has
-        // and based on that placing professor using Player Method
+    public void checkAndPlaceProfessor() throws IncorrectArgumentException{
+        int max = 0;
+        Player maxPlayer = players.getFirst();
+        for(int i=0; i< Colors.values().length; i++){
+            for(Player p : players){
+                if(p.getStudentsByColor(Colors.getColor(i))>max){
+                    maxPlayer = p;
+                }
+            }
+            maxPlayer.moveProfessor(Colors.getColor(i)); //Maybe Amrit will change the methods like this
+        }
+        //Based on AMrit changes I will probably have to tell to each player if is not the max one to remove the professor with that color
     }
 
     private void checkAndPlaceTower() throws IncorrectArgumentException { // UML this method became private
@@ -186,7 +231,7 @@ public class Game {
             if (it.hasNext()) nextTile = it.next();
             else nextTile = islands.getFirst();
             if (nextTile.getOwner().equals(currentTile.getOwner())) {
-                currentTile.sumStudentsUnification(nextTile.getStudents());
+                currentTile.addStudents(nextTile.getStudents());
                 currentTile.sumTowersUnification(nextTile.getTowers());
                 islands.remove(nextTile);
                 listChanged = true;
@@ -197,6 +242,10 @@ public class Game {
 
 
     public void checkWinner() {
-        //I need to know towers number from player, if 0 -> wins
+        for(Player p: players){
+            if(p.getPlayerTowers()==0)winner=p;
+        }
+
     }
+
 }
