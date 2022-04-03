d5d6ba469fce16a9b0202f33e4321c1e1b9e8a51
Amrit <amrit6422@gmail.com>
2022-04-03 22:40:57 +0200

    created and implemented class

---
+++ src/main/java/it/polimi/ingsw/model/StudentManager.java
@@ -0,0 +1,39 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.enumerations.Colors;
+import it.polimi.ingsw.model.exceptions.IncorrectArgumentException;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public final class StudentManager {
+    public static EnumMap<Colors, Integer> addStudent(EnumMap<Colors, Integer> students, EnumMap<Colors, Integer> studentsToAdd) {
+        for (Map.Entry<Colors, Integer> set : studentsToAdd.entrySet()) {
+            if (set.getValue() >= 0) {
+                if (students.containsKey(set.getKey())) {
+                    students.put(set.getKey(), set.getValue() + students.get(set.getKey()));
+                } else {
+                    students.put(set.getKey(), set.getValue());
+                }
+            } else {
+                return null;
+            }
+        }
+        return students;
+    }
+
+    public static EnumMap<Colors, Integer> removeStudent(EnumMap<Colors, Integer> students, EnumMap<Colors, Integer> studentsToRemove) {
+        for (Map.Entry<Colors, Integer> set : studentsToRemove.entrySet()) {
+            if (set.getValue() >= 0) {
+                if (students.containsKey(set.getKey())) {
+                    students.put(set.getKey(), students.get(set.getKey()) - set.getValue());
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        }
+        return students;
+    }
+}
