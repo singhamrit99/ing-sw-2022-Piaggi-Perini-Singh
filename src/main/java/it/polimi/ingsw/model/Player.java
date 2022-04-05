957ec4223f22a8698825207bc91f389536d7ce39
Amrit <amrit6422@gmail.com>
2022-04-05 12:31:57 +0200

    assistant card added

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -25,6 +25,7 @@ public class Player implements Comparable<Player> {
         this.nickname = nickname;
         this.schoolBoard = new SchoolBoard(numberOfPlayers);
         this.towerColor = towerColors;
+        assistantCardDeck = new AssistantCardDeck();
         assistantCardDeck.newDeck();
     }
 
