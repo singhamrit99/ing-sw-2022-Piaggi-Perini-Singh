0a14a3d8fe7bffe438b2e8fa7e2a6d7f184059b1
Amrit <amrit6422@gmail.com>
2022-04-13 13:49:59 +0200

    modified elements of interface

--- src/main/java/it/polimi/ingsw/model/deck/Deck.java
+++ src/main/java/it/polimi/ingsw/model/deck/Deck.java
@@ -11,40 +11,12 @@ public interface Deck<T> {
      */
     void fillDeck();
 
-    /**
-     * @return how many cards the deck contains
-     */
-    int getDeckSize();
-
     T get(int index);
 
     ArrayList<T> getAllCards();
 
     /**
-     * Adds a specific card to deck
-     *
-     * @param card to be added to deck
-     */
-    void addCard(T card);
-
-    /**
-     * Adds a specific group of cards to deck
-     *
-     * @param cards to be added to deck
-     */
-    void addCards(ArrayList<T> cards);
-
-    /**
      * Eliminate a specific card from deck
-     *
-     * @param card card to be eliminated
      */
-    void discardCard(T card);
-
-    /**
-     * Eliminate a specific group of cards from deck
-     *
-     * @param cards cards to be eliminated
-     */
-    void discardCards(ArrayList<T> cards);
+    void discardCard(int index);
 }
