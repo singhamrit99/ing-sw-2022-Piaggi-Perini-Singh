f944f854fde3de7ef53c230f7d8028c9628b3318
Amrit <amrit6422@gmail.com>
2022-04-13 13:47:52 +0200

    deleted not important comment

--- src/main/java/it/polimi/ingsw/model/cards/assistantcard/AssistantCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/assistantcard/AssistantCard.java
@@ -2,12 +2,8 @@ package it.polimi.ingsw.model.cards.assistantcard;
 
 import it.polimi.ingsw.model.cards.Card;
 
-/*The Assistant Card is the only card that can be played. It is defined mechanically by an integer 1 through 10 and a
- * number of tiles Mother Nature can move, dependant on the card. There are 4 copies of each card, the only difference being the wizard
- * depicted on the back.*/
-
 /**
- * @author Amrit 
+ * @author Amrit
  */
 public class AssistantCard extends Card {
     private int value;
