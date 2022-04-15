7e01cbf6e030308ddf2276c180720e0cd2000c2d
Amrit <amrit6422@gmail.com>
2022-04-15 11:43:31 +0200

    created and implemented

---
+++ src/main/java/it/polimi/ingsw/model/cards/charactercard/Ability.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.model.cards.charactercard;
+
+public class Ability {
+    private Actions action;
+    private int value;
+
+    public Ability(Actions action, int value) {
+        this.action = action;
+        this.value = value;
+    }
+
+    public enum Actions {
+        ADD_ISLAND,
+        TAKE_PROFESSORS,
+        CALCULATE_INFLUENCE,
+        MOVE_MOTHER_NATURE,
+        NO_ENTRY_TILE,
+        TOWER_INFLUENCE,
+        SWAP_ENTRANCE,
+        ADD_POINTS,
+        AVOID_COLOR_INFLUENCE,
+        SWAP_ENTRANCE_DINING,
+        ADD_DINING,
+        RETURN_STUDENT
+    }
+
+    public String getAction() {
+        return action.toString();
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
