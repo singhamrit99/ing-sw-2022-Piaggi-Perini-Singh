cda0437eaa233868754d17475b6c8e9040ef784e
Amrit <amrit6422@gmail.com>
2022-04-15 11:51:50 +0200

    implemented constructor, added attributes

--- src/main/java/it/polimi/ingsw/model/cards/charactercard/CharacterCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/charactercard/CharacterCard.java
@@ -3,10 +3,16 @@ package it.polimi.ingsw.model.cards.charactercard;
 import it.polimi.ingsw.model.cards.Card;
 
 public class CharacterCard extends Card {
-    private int startingPrice;
+    private final int startingPrice;
     private int price;
-    private String type;
     private String description;
+    private Type type;
+    private Ability ability;
+    private Requirements requirements;
+
+    public CharacterCard(int startingPrice, String description) {
+        this("", startingPrice, description);
+    }
 
     public CharacterCard(String imageName, int startingPrice, String description) {
         super(imageName);
@@ -16,8 +22,12 @@ public class CharacterCard extends Card {
         this.price = startingPrice;
     }
 
-    public CharacterCard(int startingPrice, String description) {
-        this("", startingPrice, description);
+    public CharacterCard(String imageName, int startingPrice, String description, Type type, Ability ability, Requirements requirements) {
+        this(imageName, startingPrice, description);
+
+        this.type = type;
+        this.ability = ability;
+        this.requirements = requirements;
     }
 
     public String getImageName() {
@@ -38,5 +48,20 @@ public class CharacterCard extends Card {
 
     public String getDescription() {
         return description;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Ability getAbility() {
+        return ability;
+    }
+
+    public Requirements getRequirements() {
+        return requirements;
+    }
+
+    public void activate() {
     }
 }
