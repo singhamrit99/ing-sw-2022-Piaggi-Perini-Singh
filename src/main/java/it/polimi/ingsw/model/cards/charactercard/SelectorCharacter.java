20dddc92b7246cc15a65ba517be7f2d5f65ff705
Amrit <amrit6422@gmail.com>
2022-04-15 17:31:49 +0200

    added activate method

--- src/main/java/it/polimi/ingsw/model/cards/charactercard/SelectorCharacter.java
+++ src/main/java/it/polimi/ingsw/model/cards/charactercard/SelectorCharacter.java
@@ -1,12 +1,14 @@
 package it.polimi.ingsw.model.cards.charactercard;
 
+import it.polimi.ingsw.model.Game;
+
 public class SelectorCharacter extends CharacterCard {
     public SelectorCharacter(String imageName, int startingPrice, String description, Type type, Ability ability, Requirements requirements) {
         super(imageName, startingPrice, description, type, ability, requirements);
     }
 
     @Override
-    public void activate() {
+    public void activate(Game game) {
 
     }
 }
