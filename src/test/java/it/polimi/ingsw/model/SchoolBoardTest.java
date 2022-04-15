18d56cfc11174a7fbbb79fadd4f857ed8ee8901b
Amrit <amrit6422@gmail.com>
2022-04-15 11:57:23 +0200

    warning resolved

--- src/test/java/it/polimi/ingsw/model/SchoolBoardTest.java
+++ src/test/java/it/polimi/ingsw/model/SchoolBoardTest.java
@@ -11,8 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SchoolBoardTest {
     SchoolBoard schoolBoard2 = new SchoolBoard(2);
     SchoolBoard schoolBoard3 = new SchoolBoard(3);
-
-    EnumMap<Colors, Integer> enumMap = new EnumMap<Colors, Integer>(Colors.class);
+    EnumMap<Colors, Integer> enumMap = new EnumMap<>(Colors.class);
 
     void setupEnum() {
         enumMap.put(Colors.BLUE, 2);
