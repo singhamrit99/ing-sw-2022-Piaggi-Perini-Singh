024628eded0fcbe40cec2fb76bd2db3f111f40a4
Amrit <amrit6422@gmail.com>
2022-04-15 11:52:20 +0200

    created for factory method pattern

---
+++ src/main/java/it/polimi/ingsw/model/cards/charactercard/CharacterCardFactory.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.model.cards.charactercard;
+
+import it.polimi.ingsw.model.exceptions.IncorrectArgumentException;
+
+public class CharacterCardFactory {
+    public CharacterCard getCard(String imageName, int startingPrice, String description, Type type, Ability ability, Requirements requirements) throws IncorrectArgumentException {
+        if (type.getName() == null) {
+            return null;
+        }
+        if (type.getName().equalsIgnoreCase(Type.Types.STUDENT.toString())) {
+            return new StudentCharacter(imageName, startingPrice, description, type, ability, requirements);
+        } else if (type.getName().equalsIgnoreCase(Type.Types.CONTROL.toString())) {
+            return new ControlCharacter(imageName, startingPrice, description, type, ability, requirements);
+
+        } else if (type.getName().equalsIgnoreCase(Type.Types.SELECTOR.toString())) {
+            return new SelectorCharacter(imageName, startingPrice, description, type, ability, requirements);
+        }
+
+        return null;
+    }
+}
