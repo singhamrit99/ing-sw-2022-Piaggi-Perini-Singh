502073dcd3e455ae3ab45b6fc48362942dceca5d
Amrit <amrit6422@gmail.com>
2022-04-15 17:28:09 +0200

    modified text

--- src/main/java/it/polimi/ingsw/model/exceptions/IncorrectArgumentException.java
+++ src/main/java/it/polimi/ingsw/model/exceptions/IncorrectArgumentException.java
@@ -1,11 +1,11 @@
 package it.polimi.ingsw.model.exceptions;
 
 public class IncorrectArgumentException extends Exception {
-    public IncorrectArgumentException(){
-        super("Incorrect argument");
+    public IncorrectArgumentException() {
+        super("The given parameter is incorrect");
     }
 
-    public IncorrectArgumentException(String s){
+    public IncorrectArgumentException(String s) {
         super(s);
     }
 }
