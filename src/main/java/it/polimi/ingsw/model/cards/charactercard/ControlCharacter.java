05242a3dc305272e549a34ed3d4b8bb83fa28a61
Amrit <amrit6422@gmail.com>
2022-05-02 17:05:15 +0200

    added effects

--- src/main/java/it/polimi/ingsw/model/cards/charactercard/ControlCharacter.java
+++ src/main/java/it/polimi/ingsw/model/cards/charactercard/ControlCharacter.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.model.cards.charactercard;
 
 import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.exceptions.ProfessorNotFoundException;
 
 public class ControlCharacter extends CharacterCard {
     public ControlCharacter(String imageName, int startingPrice, String description, Type type, Ability ability, Requirements requirements) {
@@ -8,7 +9,11 @@ public class ControlCharacter extends CharacterCard {
     }
 
     @Override
-    public void activate(Game game) {
+    public void activate(Game game) throws ProfessorNotFoundException {
+        Ability.Actions action = this.getAbility().getAction();
 
+        if (action.equals(Ability.Actions.TAKE_PROFESSORS)) {
+            game.checkAndPlaceProfessor();
+        }
     }
 }
--- src/main/java/it/polimi/ingsw/model/cards/charactercard/SelectorCharacter.java
+++ src/main/java/it/polimi/ingsw/model/cards/charactercard/SelectorCharacter.java
@@ -1,14 +1,43 @@
 package it.polimi.ingsw.model.cards.charactercard;
 
 import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.exceptions.NegativeValueException;
 
 public class SelectorCharacter extends CharacterCard {
+    private int noTileNumber;
+    private int choiceIndex;
+
     public SelectorCharacter(String imageName, int startingPrice, String description, Type type, Ability ability, Requirements requirements) {
         super(imageName, startingPrice, description, type, ability, requirements);
+
+        if (this.getAbility().getAction().equals(Ability.Actions.NO_ENTRY_TILE)) {
+            noTileNumber = 4;
+        } else {
+            noTileNumber = -1;
+        }
     }
 
     @Override
-    public void activate(Game game) {
+    public void activate(Game game) throws NegativeValueException {
+        Ability.Actions action = this.getAbility().getAction();
+        switch (action) {
+            case CALCULATE_INFLUENCE:
+                game.resolveMotherNature(choiceIndex);
+                break;
+            case NO_ENTRY_TILE:
+                game.getIsland(choiceIndex).setHasNoEntryTile(true);
+                setStatus(2);
+                break;
+            case RETURN_STUDENT:
+                game.returnStudentsEffect(choiceIndex);
+                break;
+            default:
+                break;
+        }
+    }
 
+    public void setChoiceIndex(int choiceIndex) {
+        this.choiceIndex = choiceIndex;
     }
 }
