8e650a518156b75ed1bc0f6cb0df6a3727e9cff2
Tino <tinopiaggi@gmail.com>
2022-05-04 16:02:48 +0200

    Skeleton of commands classes in the network_uml

--- src/main/java/it/polimi/ingsw/controller/commands/MoveMotherNature.java
+++ src/main/java/it/polimi/ingsw/controller/commands/MoveMotherNature.java
@@ -5,7 +5,7 @@ import it.polimi.ingsw.controller.Controller;
 public class MoveMotherNature implements Command {
     Controller controller;
 
-    public MoveMotherNature(Controller controller) {
+    public MoveMotherNature(Controller controller, int distance) {
         this.controller = controller;
     }
 
