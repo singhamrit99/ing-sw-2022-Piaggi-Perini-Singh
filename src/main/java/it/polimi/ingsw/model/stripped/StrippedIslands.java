e0384f3e5d634852971d9b6262d9c271102e927f
Tino <tinopiaggi@gmail.com>
2022-05-09 23:40:11 +0200

    fixed constructor StrippedIslands

--- src/main/java/it/polimi/ingsw/model/stripped/StrippedIslands.java
+++ src/main/java/it/polimi/ingsw/model/stripped/StrippedIslands.java
@@ -2,17 +2,22 @@ package it.polimi.ingsw.model.stripped;
 
 import it.polimi.ingsw.model.enumerations.Colors;
 import it.polimi.ingsw.model.enumerations.Towers;
+import it.polimi.ingsw.model.tiles.Island;
 
 import java.util.EnumMap;
 import java.util.LinkedList;
 
 public class StrippedIslands {
     private LinkedList<StrippedIsland> strippedIslands;
-    private int motherNaturePos;
 
-    public StrippedIslands(LinkedList<StrippedIsland> strippedIslands, int motherNaturePos){
-        this.motherNaturePos = motherNaturePos;
-        this.strippedIslands = strippedIslands;
+    public StrippedIslands(LinkedList<Island> islands){
+        strippedIslands = new LinkedList<>();
+        int i = 0;
+        for (Island s: islands) {
+            StrippedIsland newStrippedIsland = new StrippedIsland(s.getName(),s.getTowersColor(),
+                    s.getNumOfTowers(),s.getStudents(),s.hasMotherNature(), s.hasNoEntryTile());
+            strippedIslands.add(newStrippedIsland);
+        }
     }
 
     public LinkedList<StrippedIsland> getStrippedIslands(){
