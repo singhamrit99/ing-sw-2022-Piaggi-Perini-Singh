2aff1064864e9876e59285496e94779af3b501b4
Martino <tinopiaggi@gmail.com>
2022-05-17 12:40:17 +0200

    refactor error Exceptions

--- src/main/java/it/polimi/ingsw/exceptions/ControllerExceptions.java
+++ src/main/java/it/polimi/ingsw/exceptions/ControllerExceptions.java
@@ -1,13 +1,13 @@
 package it.polimi.ingsw.exceptions;
 
 public enum ControllerExceptions {
-    INCORRECT_ARGUMENT_EXCEPTION(101, "Game logic error! Incorrect argument provided."),
-    INCORRECT_PLAYER_EXCEPTION(102, "Game logic error! Wrong player."),
-    INCORRECT_STATE_EXCEPTION(103, "Game logic error! Incorrect phase of the game."),
-    MOTHER_NATURE_LOST_EXCEPTION(104, "Game logic error! Can't find Mother Nature."),
-    NEGATIVE_VALUE_EXCEPTION(105, "Game logic error! Negative value."),
-    PROFESSOR_NOT_FOUND_EXCEPTION(106, "Game logic error! Can't find professor."),
-    NOT_ENOUGH_COINS_EXCEPTION(107, "Game logic error! Not enough coins to play card.");
+    INCORRECT_ARGUMENT_EXCEPTION(101, "IncorrectArgument"),
+    INCORRECT_PLAYER_EXCEPTION(102, "WrongPlayer."),
+    INCORRECT_STATE_EXCEPTION(103, "Incorrect phase"),
+    MOTHER_NATURE_LOST_EXCEPTION(104, "CriticalError!MotherNature"),
+    NEGATIVE_VALUE_EXCEPTION(105, "NegativeValue"),
+    PROFESSOR_NOT_FOUND_EXCEPTION(106, "CriticalError!Professor"),
+    NOT_ENOUGH_COINS_EXCEPTION(107, "NotEnoughCoins");
     final int errorCode;
 
     ControllerExceptions(int errorCode, String message) {
