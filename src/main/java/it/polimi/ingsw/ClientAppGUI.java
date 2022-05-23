842fcadf66eda60be3cf04ba53f890f8dc953ec5
Amrit <amrit6422@gmail.com>
2022-05-23 21:24:55 +0200

    commented client.connect because it is not ready yet

--- src/main/java/it/polimi/ingsw/ClientAppGUI.java
+++ src/main/java/it/polimi/ingsw/ClientAppGUI.java
@@ -1,14 +1,15 @@
 package it.polimi.ingsw;
 
 import it.polimi.ingsw.client.Client;
-import it.polimi.ingsw.client.ViewGUI;
+import it.polimi.ingsw.client.GUI.GUI;
+
 import java.io.IOException;
 
 public class ClientAppGUI {
     public static void main(String[] args) throws IOException {
         Client client = new Client("localhost",23023);
-        client.connect();
-        ViewGUI viewGUI = new ViewGUI(client);
-        viewGUI.start();
+        //client.connect();
+        GUI gui = new GUI(client);
+        gui.start();
     }
 }
