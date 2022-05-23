a663742608dce358e8decf7508802a54eb02c5cd
Amrit <amrit6422@gmail.com>
2022-05-23 21:29:45 +0200

    pom dependencies and properties changed

---
+++ src/main/java/it/polimi/ingsw/client/GUI/ResourcesPath.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.client.GUI;
+
+/**
+ * Contains all the paths and file names needed to load a FXML file
+ */
+public interface ResourcesPath {
+    String FXML_FILE_PATH = "/fxml/"; // Equivalent to src/main/resources/fxml
+    String FILE_EXTENSION = ".fxml";
+
+    String MAIN_MENU = "MainMenu";
+
+    String IMAGES_PATH = "/img/";
+    String IMAGES_EXTENSION = ".png";
+}
