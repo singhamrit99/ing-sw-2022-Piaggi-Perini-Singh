7112a905966bd51a111a258bed5689b9d3414441
Amrit <amrit6422@gmail.com>
2022-05-24 09:41:52 +0200

    added exceptions to the method

--- src/main/java/it/polimi/ingsw/server/commands/PlayCharacterCardA.java
+++ src/main/java/it/polimi/ingsw/server/commands/PlayCharacterCardA.java
@@ -1,6 +1,10 @@
 package it.polimi.ingsw.server.commands;
 
 import it.polimi.ingsw.controller.Controller;
+import it.polimi.ingsw.exceptions.IncorrectArgumentException;
+import it.polimi.ingsw.exceptions.NegativeValueException;
+import it.polimi.ingsw.exceptions.NotEnoughCoinsException;
+import it.polimi.ingsw.exceptions.ProfessorNotFoundException;
 
 public class PlayCharacterCardA implements Command {
     int characterCardID;
@@ -15,7 +19,7 @@ public class PlayCharacterCardA implements Command {
         return playerCaller;
     }
     @Override
-    public void execute(Controller controller) {
+    public void execute(Controller controller) throws NegativeValueException, IncorrectArgumentException, ProfessorNotFoundException, NotEnoughCoinsException {
         controller.callPlayCharacterCard(characterCardID);
     }
 }
