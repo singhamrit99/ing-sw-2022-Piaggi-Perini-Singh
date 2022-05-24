1298b6ff62d42d94be2c15ca5dd6b6e7be7df1bf
Amrit <amrit6422@gmail.com>
2022-05-24 09:37:22 +0200

    added thrown exceptions

--- src/main/java/it/polimi/ingsw/server/commands/Command.java
+++ src/main/java/it/polimi/ingsw/server/commands/Command.java
@@ -1,10 +1,11 @@
 package it.polimi.ingsw.server.commands;
 
 import it.polimi.ingsw.controller.Controller;
+import it.polimi.ingsw.exceptions.*;
 
 
 public interface Command {
 
     String getCaller();
-    void execute(Controller controller);
+    void execute(Controller controller) throws IncorrectPlayerException, NegativeValueException, IncorrectArgumentException, MotherNatureLostException, IncorrectStateException, ProfessorNotFoundException, NotEnoughCoinsException, AssistantCardNotFound;
 }
