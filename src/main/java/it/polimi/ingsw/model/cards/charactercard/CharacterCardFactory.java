81700c6209ce6838c76df892a055eeed3621c42f
Amrit <amrit6422@gmail.com>
2022-05-26 23:06:38 +0200

    implemented serializable

--- src/main/java/it/polimi/ingsw/model/cards/charactercard/CharacterCardFactory.java
+++ src/main/java/it/polimi/ingsw/model/cards/charactercard/CharacterCardFactory.java
@@ -2,8 +2,14 @@ package it.polimi.ingsw.model.cards.charactercard;
 
 import it.polimi.ingsw.exceptions.IncorrectArgumentException;
 import it.polimi.ingsw.exceptions.NegativeValueException;
+import it.polimi.ingsw.model.enumerations.Types;
 
-public class CharacterCardFactory {
+import java.io.Serializable;
+
+/**
+ * @author Amrit
+ */
+public class CharacterCardFactory implements Serializable {
     public CharacterCard getCard(String imageName, int startingPrice, String description, Type type, Ability ability, Requirements requirements) throws IncorrectArgumentException, NegativeValueException {
         if (type.getName() == null) {
             return null;
