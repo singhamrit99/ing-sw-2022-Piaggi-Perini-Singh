5003b1075dd4eee1bff149bb464710929d6b19f8
Amrit <amrit6422@gmail.com>
2022-05-26 23:05:39 +0200

    created enum

---
+++ src/main/java/it/polimi/ingsw/model/enumerations/Actions.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.model.enumerations;
+
+import java.io.Serializable;
+
+public enum Actions implements Serializable {
+    ADD_ISLAND,
+    TAKE_PROFESSORS,
+    CALCULATE_INFLUENCE,
+    MOVE_MOTHER_NATURE,
+    NO_ENTRY_TILE,
+    TOWER_INFLUENCE,
+    SWAP_ENTRANCE,
+    ADD_POINTS,
+    AVOID_COLOR_INFLUENCE,
+    SWAP_ENTRANCE_DINING,
+    ADD_DINING,
+    RETURN_STUDENT
+}
