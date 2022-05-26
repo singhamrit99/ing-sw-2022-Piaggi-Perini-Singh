191b5a4fc236bef685290ab856b6e537a79bb18d
Martino <tinopiaggi@gmail.com>
2022-05-26 19:30:37 +0200

    refactor to support new Exceptions

--- src/main/java/it/polimi/ingsw/client/Client.java
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -5,23 +5,25 @@ import it.polimi.ingsw.model.stripped.StrippedModel;
 import it.polimi.ingsw.server.commands.Command;
 import it.polimi.ingsw.server.commands.PlayCharacterCardA;
 import it.polimi.ingsw.server.serverStub;
+
 import java.beans.PropertyChangeEvent;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
 
-public class Client implements Runnable{
+public class Client implements Runnable {
     final private String ip;
     final private int port;
     private String nickname;
-    private String clientRoom= null;
+    private String clientRoom = null;
     private ArrayList<String> roomList;
     private serverStub server;
     private boolean inGame;
     private StrippedModel localModel;
     private boolean localModelLoaded;
     private boolean userRegistered;
+
     public Client(String ip, int port) {
         this.ip = ip;
         this.port = port;
@@ -29,6 +31,7 @@ public class Client implements Runnable{
         localModelLoaded = false;
         userRegistered = false;
     }
+
     public void connect() {
         try {
             Registry registry = LocateRegistry.getRegistry(ip, port);
@@ -39,92 +42,100 @@ public class Client implements Runnable{
             e.printStackTrace();
         }
     }
+
     public void registerClient(String nickName) throws RemoteException, UserAlreadyExistsException {
         server.registerUser(nickName);
         this.nickname = nickName;
-        userRegistered=true;
-        new Thread(this).start();
-        //it's important that the thread runs only after the correct registration!
+        userRegistered = true;
+        new Thread(this).start(); //it's important that the thread runs only after the correct registration!
     }
 
-    public void deregisterClient() throws RemoteException{
-        if(userRegistered){
+    public void deregisterClient() throws RemoteException, UserNotRegisteredException {
+        if (userRegistered) {
             server.deregisterConnection(nickname);
             this.nickname = null;
-            userRegistered=false;
+            userRegistered = false;
         }
     }
-    public void createRoom(String roomName) throws RemoteException {
+
+    public void createRoom(String roomName) throws RemoteException, UserNotRegisteredException, RoomAlreadyExistsException {
         server.createRoom(nickname, roomName);
-        clientRoom=roomName;
+        clientRoom = roomName;
     }
-    public void requestRoomJoin(String roomName) throws RemoteException {
+
+    public void requestRoomJoin(String roomName) throws RemoteException, RoomNotExistsException, UserInRoomException, UserNotRegisteredException {
         server.joinRoom(nickname, roomName);
-        clientRoom= roomName;
+        clientRoom = roomName;
 
     }
-    public ArrayList<String> requestLobbyInfo(String roomName) throws RemoteException {
+
+    public ArrayList<String> requestLobbyInfo(String roomName) throws RemoteException, RoomNotExistsException {
         return server.getLobbyInfo(roomName);
     }
+
     public ArrayList<String> getRooms() throws RemoteException {
         return server.getRoomsList();
     }
-    public ArrayList<String> getNicknamesInRoom(String roomName) throws RemoteException {
+
+    public ArrayList<String> getNicknamesInRoom(String roomName) throws RemoteException, RoomNotExistsException {
         return server.getPlayers(roomName);
     }
-    public void setExpertMode(boolean value) throws RemoteException, NotLeaderRoomException, UserNotInRoomException {
+
+    public void setExpertMode(boolean value) throws RemoteException, NotLeaderRoomException, UserNotInRoomException, UserNotRegisteredException {
         server.setExpertMode(nickname, value);
     }
-    public void leaveRoom() throws RemoteException, UserNotInRoomException {
-        if (clientRoom==null)
-        {
+
+    public void leaveRoom() throws RemoteException, UserNotInRoomException, UserNotRegisteredException {
+        if (clientRoom == null) {
             throw new UserNotInRoomException();
-        }
-        else {
+        } else {
             server.leaveRoom(nickname);
             clientRoom = null;
         }
     }
 
-    public boolean isLeader() throws RemoteException {
+    public boolean isLeader() throws RemoteException, RoomNotExistsException {
         return getNicknamesInRoom(clientRoom).get(0).equals(nickname);
     }
 
-    public void startGame() throws  RemoteException, NotLeaderRoomException, UserNotInRoomException {
+    public void startGame() throws RemoteException, NotLeaderRoomException, UserNotInRoomException, RoomNotExistsException, UserNotRegisteredException {
         server.startGame(nickname);
     }
 
-
-    @Override //TODO DEPRECATED CODE, REPLACE WITH THREAD POOL EXECUTOR
+    @Override
     public void run() {
-         while (userRegistered) {
+        while (userRegistered) {
             try {
-                if(inGame){
-                    System.out.println("the room is playing");
+                if (inGame) {
+                    System.out.println("the room is playing and I'm in");
                     ArrayList<PropertyChangeEvent> newUpdates = server.getUpdates(nickname);
                     manageUpdates(newUpdates);
+                } else {
+                    try {
+                        inGame = server.inGame(nickname);
+                        roomList = server.getRoomsList();
+                    } catch (UserNotRegisteredException notRegisteredException) {
+                        userRegistered = false;
+                        break;
+                    }
                 }
-                else {
-                    inGame=server.inGame(nickname);
-                    roomList = server.getRoomsList();
-                }
-
-                        Ping();
-
+                Ping();
                 Thread.sleep(500); //should be half of server ping timeout
-            } catch (RemoteException | InterruptedException | LocalModelNotLoadedException e) {
+            } catch (RemoteException | InterruptedException |
+                     LocalModelNotLoadedException | UserNotInRoomException |
+                     UserNotRegisteredException e) {
                 System.err.println("Client exception: " + e);
             }
         }
     }
 
-    private void Ping() throws  RemoteException{
+    private void Ping() throws RemoteException, UserNotRegisteredException {
         server.ping(nickname);
-       //System.out.println("Ho pingato il server");
     }
+
     private void manageUpdates(ArrayList<PropertyChangeEvent> evtArray) throws LocalModelNotLoadedException {
-        for(PropertyChangeEvent evt: evtArray){
-            switch (evt.getPropertyName()){
+        for (PropertyChangeEvent evt : evtArray) {
+            switch (evt.getPropertyName()) {
                 case "init":
                     localModel = (StrippedModel) evt.getNewValue();
                     break;
@@ -132,10 +143,9 @@ public class Client implements Runnable{
                     //notify message to view TODO
                     break;
                 default:
-                    if(localModel!=null){
+                    if (localModel != null) {
                         localModel.updateModel(evt);
-                    }
-                    else{
+                    } else {
                         throw new LocalModelNotLoadedException();
                     }
                     break;
@@ -143,7 +153,9 @@ public class Client implements Runnable{
         }
     }
 
-    public void performGameAction(Command command) throws NotEnoughCoinsException, AssistantCardNotFoundException, NegativeValueException, IncorrectStateException, MotherNatureLostException, ProfessorNotFoundException, IncorrectPlayerException, RemoteException, IncorrectArgumentException {
-        server.performGameAction(command);
+    public void performGameAction(Command command) throws NotEnoughCoinsException, AssistantCardNotFoundException, NegativeValueException,
+            IncorrectStateException, MotherNatureLostException, ProfessorNotFoundException, IncorrectPlayerException, RemoteException, IncorrectArgumentException,
+            UserNotInRoomException, UserNotRegisteredException {
+        server.performGameAction(nickname, command);
     }
 }
