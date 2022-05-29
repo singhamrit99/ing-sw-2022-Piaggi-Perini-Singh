3f65aeb8b504fc0b97d43d1dda4e1bda321b9650
Amrit <amrit6422@gmail.com>
2022-05-29 10:04:09 +0200

    implemented roomsAvailable method

--- src/main/java/it/polimi/ingsw/client/GUI/GUI.java
+++ src/main/java/it/polimi/ingsw/client/GUI/GUI.java
@@ -1,13 +1,25 @@
 package it.polimi.ingsw.client.GUI;
 
 import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.client.GUI.controller.Controller;
+import it.polimi.ingsw.client.GUI.controller.ResourcesPath;
+import it.polimi.ingsw.client.GUI.controller.RoomListController;
 import javafx.application.Application;
+import javafx.application.Platform;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class GUI {
     public static Client client;
+    public static Controller controller;
+
+    private final AtomicBoolean isDoing;
 
     public GUI(Client client) {
+        this.isDoing = new AtomicBoolean(false);
         GUI.client = client;
+        GUI.client.setGUI(this);
     }
 
     /**
@@ -15,5 +27,36 @@ public class GUI {
      */
     public void start() {
         Application.launch(GUILauncher.class);
+    }
+
+    public void startAction() {
+        if (!isDoing.get()) {
+            isDoing.set(true);
+        }
+    }
+
+    public void stopAction() {
+        if (isDoing.get()) {
+            isDoing.set(false);
+        }
+    }
+
+    public boolean isDoing() {
+        return this.isDoing.get();
+    }
+
+    /**
+     * Starts the window that shows the list of rooms available
+     *
+     * @param rooms list of rooms
+     */
+    public void roomsAvailable(ArrayList<String> rooms) {
+        Platform.runLater(() -> {
+            RoomListController roomListController = new RoomListController(this);
+            roomListController.setRoomsList(rooms);
+
+            Controller.startStage(ResourcesPath.ROOM_LIST, roomListController);
+            controller.closeStage();
+        });
     }
 }
