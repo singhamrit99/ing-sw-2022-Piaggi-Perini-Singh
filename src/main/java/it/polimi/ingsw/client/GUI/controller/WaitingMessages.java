dcaa3b718b5734e49c5eb3c0f4c64113357e3f3b
Amrit <amrit6422@gmail.com>
2022-05-29 10:07:15 +0200

    created interface

---
+++ src/main/java/it/polimi/ingsw/client/GUI/controller/WaitingMessages.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.client.GUI.controller;
+
+/**
+ * @author Amrit
+ * Contains all the messages that can be shown in a waiting window
+ */
+public interface WaitingMessages {
+    String CONNECTING = "Connecting...";
+}
