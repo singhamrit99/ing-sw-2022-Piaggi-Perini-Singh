e2c86e48972ca669adf60e57b5d3b9992ebe594f
Amrit <amrit6422@gmail.com>
2022-05-29 10:02:04 +0200

    refactored

--- src/main/java/it/polimi/ingsw/ClientAppGUI.java
+++ src/main/java/it/polimi/ingsw/ClientAppGUI.java
@@ -7,9 +7,10 @@ import java.io.IOException;
 
 public class ClientAppGUI {
     public static void main(String[] args) throws IOException {
-        Client client = new Client("localhost",23023);
+        Client client = new Client("localhost", 23023);
         client.connect();
         client.run();
+
         GUI gui = new GUI(client);
         gui.start();
     }
