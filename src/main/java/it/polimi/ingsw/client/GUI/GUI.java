dc95176b82f8c9618ba00e43a3b966aca2b3a740
Amrit <amrit6422@gmail.com>
2022-05-31 09:22:08 +0200

    implemented interface

--- src/main/java/it/polimi/ingsw/client/GUI/GUI.java
+++ src/main/java/it/polimi/ingsw/client/GUI/GUI.java
@@ -8,10 +8,12 @@ import it.polimi.ingsw.client.UI;
 import javafx.application.Application;
 import javafx.application.Platform;
 
+import java.beans.PropertyChangeEvent;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class GUI{
+public class GUI implements UI{
     public static Client client;
     public static Controller controller;
 
@@ -20,7 +22,7 @@ public class GUI{
     public GUI(Client client) {
         this.isDoing = new AtomicBoolean(false);
         GUI.client = client;
-        //GUI.client.setUI(this);
+        GUI.client.setUI(this);
     }
 
     /**
@@ -44,6 +46,76 @@ public class GUI{
 
     public boolean isDoing() {
         return this.isDoing.get();
+    }
+
+    @Override
+    public void startGame() throws RemoteException {
+
+    }
+
+    @Override
+    public void currentPlayer(String s) {
+
+    }
+
+    @Override
+    public void notifyCloud(PropertyChangeEvent e) {
+
+    }
+
+    @Override
+    public void deckChange(String input) {
+
+    }
+
+    @Override
+    public void assistantCardPlayed(PropertyChangeEvent e) {
+
+    }
+
+    @Override
+    public void islandChange(PropertyChangeEvent e) {
+
+    }
+
+    @Override
+    public void islandMerged(PropertyChangeEvent e) {
+
+    }
+
+    @Override
+    public void islandConquest(PropertyChangeEvent e) {
+
+    }
+
+    @Override
+    public void diningChange(PropertyChangeEvent e) {
+
+    }
+
+    @Override
+    public void towersEvent(PropertyChangeEvent e) {
+
+    }
+
+    @Override
+    public void gameOver(String winner) {
+
+    }
+
+    @Override
+    public void coinsChanged(PropertyChangeEvent e) {
+
+    }
+
+    @Override
+    public void entranceChanged(PropertyChangeEvent e) {
+
+    }
+
+    @Override
+    public void removedProfessors(PropertyChangeEvent e) {
+
     }
 
     /**
