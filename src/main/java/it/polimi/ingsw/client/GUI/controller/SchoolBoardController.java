a64719d0e838df0f64456508cc705c5e8e27b3c3
Amrit <amrit6422@gmail.com>
2022-06-02 14:55:25 +0200

    added schoolBoardController class

---
+++ src/main/java/it/polimi/ingsw/client/GUI/controller/SchoolBoardController.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.client.GUI.controller;
+
+import it.polimi.ingsw.client.GUI.GUI;
+
+public class SchoolBoardController extends InitialStage implements Controller{
+    public SchoolBoardController(GUI gui) {
+        super(gui);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+
+}
