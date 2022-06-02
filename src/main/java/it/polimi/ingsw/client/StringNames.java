55d5357af0354e606fcd1b872d907e92d0167825
Amrit <amrit6422@gmail.com>
2022-06-02 21:59:39 +0200

    added strings to improve code

---
+++ src/main/java/it/polimi/ingsw/client/StringNames.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.client;
+
+/**
+ * @author Amrit
+ */
+public interface StringNames {
+    //used for various type of display displaying
+    String TITLE = "Eryantis";
+    String JOIN = "Join";
+
+    //used for view definition
+    String LOBBY = "Lobby";
+    String LAUNCHER = "Launcher";
+    String ROOM = "Room";
+    String BOARD = "Board";
+    String CREATE_NEW_ROOM = "CreateRoom";
+
+    //used for alerts
+    String NICKNAME_ALREADY_EXISTS = "Sorry! Nickname is already taken";
+    String CONNECTION_ERROR = "Sorry! There is an issue with the connection";
+    String NICKNAME_FIELD_NULL = "Nickname field is not filled";
+    String NO_SUCH_ROOM = "No such room found";
+    String USER_NOT_REGISTERED = "User is not registered in the server";
+    String NO_LEADER = "You don't have permission to perform this action";
+    String NOT_IN_ROOM = "You are not in the room";
+    String ROOM_EXISTS = "Sorry! Room name already taken";
+}
