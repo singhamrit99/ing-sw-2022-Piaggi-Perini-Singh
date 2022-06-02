5178747f4ae84187e165f26016a1ddafe471c0f5
Amrit <amrit6422@gmail.com>
2022-06-02 21:58:02 +0200

    added alert

--- src/main/java/it/polimi/ingsw/client/GUI/GUILauncher.java
+++ src/main/java/it/polimi/ingsw/client/GUI/GUILauncher.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.client.GUI;
 
 import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.client.GUI.controller.ResourcesPath;
+import it.polimi.ingsw.client.StringNames;
+import it.polimi.ingsw.client.GUI.controller.Utility;
 import it.polimi.ingsw.exceptions.UserAlreadyExistsException;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -10,7 +12,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -22,6 +23,9 @@ import java.net.URL;
 import java.rmi.RemoteException;
 import java.util.ResourceBundle;
 
+/**
+ * @author Amrit
+ */
 public class GUILauncher extends Application implements Initializable {
     public static Client client = GUI.client;
     public static Stage mainWindow;
@@ -38,7 +42,7 @@ public class GUILauncher extends Application implements Initializable {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Eriantys");
+        stage.setTitle(StringNames.TITLE);
         stage.setResizable(false);
         stage.setWidth(MAIN_MENU_WIDTH);
         stage.setHeight(MAIN_MENU_HEIGHT);
@@ -52,7 +56,7 @@ public class GUILauncher extends Application implements Initializable {
             System.exit(0);
         });
 
-        String path = ResourcesPath.FXML_FILE_PATH + ResourcesPath.MAIN_MENU + ResourcesPath.FILE_EXTENSION;
+        String path = ResourcesPath.FXML_FILE_PATH + ResourcesPath.LAUNCHER + ResourcesPath.FILE_EXTENSION;
         Parent root = FXMLLoader.load(getClass().getResource(path));
         Scene scene = new Scene(root);
         stage.setScene(scene);
@@ -61,50 +65,31 @@ public class GUILauncher extends Application implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //ErrorManager.initializeElements(nicknameField);
         startButtonEvent();
     }
 
-    /**
-     * Sets the actions to do when start button is clicked
-     */
     private void startButtonEvent() {
         startButton.setOnAction((event) -> {
             if (controlNickname()) {
                 final String nickname = nicknameField.getText();
 
                 try {
-                    GUI.view = "lobby";
+                    GUI.client.view = StringNames.LOBBY;
                     client.registerClient(nickname);
                 } catch (UserAlreadyExistsException e) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error");
-                    alert.setHeaderText("Nickname error");
-                    alert.setContentText("Sorry! Username is already taken");
-
-                    alert.showAndWait();
+                    Utility.showErrorDialogBox(StringNames.NICKNAME_ALREADY_EXISTS);
                 } catch (RemoteException e) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error");
-                    alert.setHeaderText("Network error");
-                    alert.setContentText("Sorry! There is an issue with the connection");
-
-                    alert.showAndWait();
+                    Utility.showErrorDialogBox(StringNames.CONNECTION_ERROR);
                 }
             }
         });
     }
 
-    /**
-     * Controls choices before sent them to server
-     *
-     * @return true if choices are correct
-     */
     private boolean controlNickname() {
         final String nickname = nicknameField.getText();
 
         if (nickname.length() == 0) {
-            //ErrorManager.setError(nicknameField);
+            Utility.showErrorDialogBox(StringNames.NICKNAME_FIELD_NULL);
             return false;
         }
         return true;
