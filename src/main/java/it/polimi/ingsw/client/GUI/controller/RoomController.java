dcb8ddb89cb0fba0ac591f353d0940ea8760d133
Amrit <amrit6422@gmail.com>
2022-06-05 13:00:23 +0200

    Switch Button implemented for expert mode

--- src/main/java/it/polimi/ingsw/client/GUI/controller/RoomController.java
+++ src/main/java/it/polimi/ingsw/client/GUI/controller/RoomController.java
@@ -6,10 +6,11 @@ import it.polimi.ingsw.exceptions.*;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.scene.control.Button;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Text;
 
@@ -25,30 +26,42 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class RoomController extends InitialStage implements Controller {
     private ArrayList<String> players = new ArrayList<>();
     protected static AtomicBoolean opened = new AtomicBoolean(false);
+
+    @FXML
+    private HBox expertBox;
     @FXML
     private GridPane playersList;
     @FXML
     private Text roomTitle;
-
     @FXML
     private Button startGameButton;
     @FXML
     private Button leaveButton;
-    @FXML
-    private ToggleButton setExpertMode;
-
 
     private Image blackTowerImage;
     private Image whiteTowerImage;
     private Image greyTowerImage;
+
+    private SwitchButton setExpertMode;
+    private Label expertMode;
+
     public RoomController(GUI gui) {
         super(gui);
     }
+
     @FXML
     public void initialize() {
         opened.set(true);
         roomTitle.setText(GUI.client.getRoom());
-        setExpertMode = new ToggleButton();
+
+        expertMode = new Label();
+        expertMode.setLabelFor(setExpertMode);
+        setExpertMode = new SwitchButton();
+        setExpertMode.setMaxHeight(20);
+        setExpertMode.setMaxWidth(20);
+
+        expertBox.getChildren().add(expertMode);
+        expertBox.getChildren().add(setExpertMode);
 
         try {
             blackTowerImage = new Image(new FileInputStream(ResourcesPath.BLACK_TOWER));
@@ -83,20 +96,9 @@ public class RoomController extends InitialStage implements Controller {
         try {
             if (GUI.client.isLeader()) {
                 startGameButton.setVisible(true);
-
-                setExpertMode.setOnAction((event) -> {
-                    try {
-                        GUI.client.setExpertMode(setExpertMode.selectedProperty().get());
-                    } catch (RemoteException e) {
-                        Utility.showErrorDialogBox(StringNames.CONNECTION_ERROR);
-                    } catch (NotLeaderRoomException e) {
-                        Utility.showErrorDialogBox(StringNames.NO_LEADER);
-                    } catch (UserNotInRoomException e) {
-                        Utility.showErrorDialogBox(StringNames.NOT_IN_ROOM);
-                    } catch (UserNotRegisteredException e) {
-                        Utility.showErrorDialogBox(StringNames.USER_NOT_REGISTERED);
-                    }
-                });
+                setExpertMode.setVisible(true);
+                setExpertMode.changeState(GUI.client.getExpertMode());
+                expertMode.setText(StringNames.EXPERT_MODE);
 
                 startGameButton.setOnAction((event) -> {
                     opened.set(false);
@@ -116,8 +118,29 @@ public class RoomController extends InitialStage implements Controller {
                         Utility.showErrorDialogBox(StringNames.ALONE_IN_ROOM);
                     }
                 });
+
+                setExpertMode.setOnMouseClicked(mouseEvent -> {
+                    try {
+                        setExpertMode.changeState(!setExpertMode.getState());
+                        GUI.client.setExpertMode(setExpertMode.getState());
+                    } catch (RemoteException e) {
+                        Utility.showErrorDialogBox(StringNames.CONNECTION_ERROR);
+                    } catch (NotLeaderRoomException e) {
+                        Utility.showErrorDialogBox(StringNames.NO_LEADER);
+                    } catch (UserNotInRoomException e) {
+                        Utility.showErrorDialogBox(StringNames.NOT_IN_ROOM);
+                    } catch (UserNotRegisteredException e) {
+                        Utility.showErrorDialogBox(StringNames.USER_NOT_REGISTERED);
+                    }
+                });
             } else {
                 startGameButton.setVisible(false);
+                setExpertMode.setVisible(false);
+                if (GUI.client.getExpertMode()) {
+                    expertMode.setText(StringNames.EXPERT_MODE_ON);
+                } else {
+                    expertMode.setText(StringNames.EXPERT_MODE_OFF);
+                }
             }
         } catch (RemoteException e) {
             Utility.showErrorDialogBox(StringNames.CONNECTION_ERROR);
@@ -130,8 +153,6 @@ public class RoomController extends InitialStage implements Controller {
         for (int i = 0; i < players.size(); i++) {
             ImageView team = new ImageView();
             if (players.size() == 3) {
-                //TODO AMRIT: do randomly selected ;
-                // todo TINO: nah, we would have to change the model at serverSide
                 if (i == 0) team = new ImageView(blackTowerImage);
                 if (i == 1) team = new ImageView(whiteTowerImage);
                 if (i == 2) team = new ImageView(greyTowerImage);
