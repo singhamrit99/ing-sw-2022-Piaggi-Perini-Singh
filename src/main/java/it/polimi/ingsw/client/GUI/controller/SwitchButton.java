1c37570e26c457e3084aa465d884f4f49d9d2c62
Amrit <amrit6422@gmail.com>
2022-06-05 12:55:54 +0200

    Switch Button created

---
+++ src/main/java/it/polimi/ingsw/client/GUI/controller/SwitchButton.java
@@ -0,0 +1,65 @@
+package it.polimi.ingsw.client.GUI.controller;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+public class SwitchButton extends StackPane {
+    private final Rectangle back = new Rectangle(30, 10, Color.RED);
+    private final Button button = new Button();
+    private final String buttonStyleOff = "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.2), 0.2, 0.0, 0.0, 2); -fx-background-color: WHITE;";
+    private final String buttonStyleOn = "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.2), 0.2, 0.0, 0.0, 2); -fx-background-color: #00893d;";
+    private boolean state;
+
+    private void init() {
+        getChildren().addAll(back, button);
+        setMinSize(30, 15);
+        back.maxWidth(30);
+        back.minWidth(30);
+        back.maxHeight(10);
+        back.minHeight(10);
+        back.setArcHeight(back.getHeight());
+        back.setArcWidth(back.getHeight());
+        back.setFill(Color.valueOf("#ced5da"));
+        double r = 2.0;
+        button.setShape(new Circle(r));
+        setAlignment(button, Pos.CENTER_LEFT);
+        button.setMaxSize(15, 15);
+        button.setMinSize(15, 15);
+        button.setStyle(buttonStyleOff);
+        state = false;
+    }
+
+    public SwitchButton() {
+        init();
+        button.setFocusTraversable(false);
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public boolean getState() {
+        return state;
+    }
+
+    public void changeState(boolean state) {
+        this.state = state;
+        setButtonStyle();
+    }
+
+    private void setButtonStyle() {
+        if (state) {
+            button.setStyle(buttonStyleOn);
+            back.setFill(Color.valueOf("#80C49E"));
+            setAlignment(button, Pos.CENTER_RIGHT);
+        } else {
+            button.setStyle(buttonStyleOff);
+            back.setFill(Color.valueOf("#ced5da"));
+            setAlignment(button, Pos.CENTER_LEFT);
+        }
+    }
+}
