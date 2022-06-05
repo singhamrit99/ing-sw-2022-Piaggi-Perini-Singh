ff074738dde0e8d12e0945f073b4f9827f72d81e
Amrit <amrit6422@gmail.com>
2022-06-05 21:07:32 +0200

    added removeCoins test

--- src/test/java/it/polimi/ingsw/model/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/model/PlayerTest.java
@@ -1,32 +1,29 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.exceptions.IncorrectArgumentException;
+import it.polimi.ingsw.exceptions.NegativeValueException;
+import it.polimi.ingsw.model.enumerations.Towers;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    Player player = new Player("Luffy", Towers.BLACK, 2);
 
     @Test
-    void playAssistantCard() {
-    }
+    void testRemoveCoins() {
+        assertEquals(1, player.getCoins());
+        assertThrows(NegativeValueException.class, () -> player.removeCoins(-2));
 
-    @Test
-    void moveMotherNature() {
-    }
+        try {
+            player.removeCoins(1);
+        } catch (NegativeValueException e) {
+            e.printStackTrace();
+        } catch (IncorrectArgumentException e) {
+            e.printStackTrace();
+        }
 
-    @Test
-    void moveStudents() {
-    }
-
-    @Test
-    void compareTo() {
-    }
-
-    @Test
-    void moveTowers() {
-    }
-
-    @Test
-    void hasProfessorOfColor() {
+        assertEquals(0, player.getCoins());
+        assertThrows(IncorrectArgumentException.class, () -> player.removeCoins(1));
     }
 }
