bd5f87132b035072233dc6d4f8e2f3697f90571b
Amrit <amrit6422@gmail.com>
2022-06-05 21:04:30 +0200

    code refactored

--- src/main/java/it/polimi/ingsw/model/cards/charactercard/CharacterCardFactory.java
+++ src/main/java/it/polimi/ingsw/model/cards/charactercard/CharacterCardFactory.java
@@ -13,12 +13,10 @@ public class CharacterCardFactory implements Serializable {
     public CharacterCard getCard(String imageName, int startingPrice, String description, Type type, Ability ability, Requirements requirements) throws IncorrectArgumentException, NegativeValueException {
         if (type.getName() == null) {
             return null;
-        }
-        if (type.getName().equalsIgnoreCase(Types.STUDENT.toString())) {
+        } else if (type.getName().equalsIgnoreCase(Types.STUDENT.toString())) {
             return new StudentCharacter(imageName, startingPrice, description, type, ability, requirements);
         } else if (type.getName().equalsIgnoreCase(Types.CONTROL.toString())) {
             return new ControlCharacter(imageName, startingPrice, description, type, ability, requirements);
-
         } else if (type.getName().equalsIgnoreCase(Types.SELECTOR.toString())) {
             return new SelectorCharacter(imageName, startingPrice, description, type, ability, requirements);
         }
