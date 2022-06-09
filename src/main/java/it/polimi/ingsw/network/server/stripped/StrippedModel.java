3e2ab5ca8a309a7b9248d0296d430ddd69e992ef
Amrit <amrit6422@gmail.com>
2022-06-09 18:30:08 +0200

    added break

--- src/main/java/it/polimi/ingsw/network/server/stripped/StrippedModel.java
+++ src/main/java/it/polimi/ingsw/network/server/stripped/StrippedModel.java
@@ -60,9 +60,10 @@ public class StrippedModel implements Serializable {
                 changeAssistantDeck(evt);
                 break;
             case "current-player":
+                break;
             case "first-player":
-                ui.currentPlayer((String)evt.getNewValue());
-                setCurrentPlayer((String)evt.getNewValue()); //todo remove
+                ui.currentPlayer((String) evt.getNewValue());
+                setCurrentPlayer((String) evt.getNewValue()); //todo remove
                 break;
             case "game-over":
                 winnerTeam = (String) evt.getNewValue();
@@ -77,11 +78,11 @@ public class StrippedModel implements Serializable {
     private void changeAssistantDeck(PropertyChangeEvent evt) {
         String ownerDeck = currentPlayer;
         Optional<AssistantCardDeck> optionalDeckToModify = assistantDecks.stream().filter(d -> d.getOwner().equals(ownerDeck)).findFirst();
-        AssistantCardDeck deckToModify= optionalDeckToModify.get();
+        AssistantCardDeck deckToModify = optionalDeckToModify.get();
         if (optionalDeckToModify.isPresent()) {
             assistantDecks.remove(deckToModify);
             assistantDecks.add((AssistantCardDeck) evt.getNewValue());
-            String playedCard=(String) evt.getOldValue();
+            String playedCard = (String) evt.getOldValue();
             ui.deckChange(playedCard);
         }
     }
@@ -143,19 +144,16 @@ public class StrippedModel implements Serializable {
     private void changeIsland(PropertyChangeEvent evt) {
         StrippedIsland changedIsland = (StrippedIsland) evt.getOldValue();
         Optional<StrippedIsland> optionalIslandFound = islands.stream().filter(x -> x.getName().equals(changedIsland.getName())).findFirst();
-        StrippedIsland islandFound= optionalIslandFound.get();
+        StrippedIsland islandFound = optionalIslandFound.get();
         if (optionalIslandFound.isPresent()) {
             islands.remove(islandFound); //IslandEvent Deletion
             if (evt.getNewValue() != null) {
                 islands.add((StrippedIsland) evt.getNewValue());
-                if(evt.getPropertyName().equals("island"))
-                ui.islandChange(evt);
-                else if(evt.getPropertyName().equals("island-conquest"))
-                {
+                if (evt.getPropertyName().equals("island"))
+                    ui.islandChange(evt);
+                else if (evt.getPropertyName().equals("island-conquest")) {
                     ui.islandConquest(evt);
-                }
-                else if(evt.getPropertyName().equals("island-merged"))
-                {
+                } else if (evt.getPropertyName().equals("island-merged")) {
                     ui.islandMerged(evt);
                 }
             }
@@ -174,7 +172,7 @@ public class StrippedModel implements Serializable {
             ui.notifyCloud(evt);
         }
         Optional<StrippedCloud> optionalCloudFound = clouds.stream().filter(x -> x.getName().equals(changedCloud.getName())).findFirst();
-        StrippedCloud cloudFound= optionalCloudFound.get();
+        StrippedCloud cloudFound = optionalCloudFound.get();
         clouds.remove(cloudFound);
         clouds.add(changedCloud);
     }
@@ -204,8 +202,8 @@ public class StrippedModel implements Serializable {
     }
 
     public StrippedBoard getBoardOf(String owner) throws LocalModelNotLoadedException {
-        for (StrippedBoard b : boards){
-            if(b.getOwner().equals(owner))return b;
+        for (StrippedBoard b : boards) {
+            if (b.getOwner().equals(owner)) return b;
         }
         throw new LocalModelNotLoadedException(); //todo change the name of this exception with something more specific
     }
