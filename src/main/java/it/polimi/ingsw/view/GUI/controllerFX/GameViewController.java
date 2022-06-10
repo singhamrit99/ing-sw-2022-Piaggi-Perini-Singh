593b2b64b290f206df3849c6203b6be8a4e69bd1
Amrit <amrit6422@gmail.com>
2022-06-10 21:40:02 +0200

    added check in bag

--- src/main/java/it/polimi/ingsw/view/GUI/controllerFX/GameViewController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/controllerFX/GameViewController.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.view.GUI.controllerFX;
 import it.polimi.ingsw.StringNames;
 import it.polimi.ingsw.exceptions.*;
 import it.polimi.ingsw.model.enumerations.Colors;
+import it.polimi.ingsw.model.enumerations.State;
+import it.polimi.ingsw.network.server.commands.DrawFromBagCommand;
 import it.polimi.ingsw.network.server.stripped.StrippedBoard;
 import it.polimi.ingsw.network.server.stripped.StrippedCloud;
 import it.polimi.ingsw.view.GUI.GUI;
@@ -10,9 +12,7 @@ import javafx.animation.Interpolator;
 import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.geometry.HPos;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
@@ -28,7 +28,6 @@ import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class GameViewController extends InitialStage implements Controller {
@@ -82,8 +81,8 @@ public class GameViewController extends InitialStage implements Controller {
 
 
         //animations
-        if(islandsPanes.size()!=0){
-            for(Pane island : islandsPanes){
+        if (islandsPanes.size() != 0) {
+            for (Pane island : islandsPanes) {
                 TranslateTransition floatingTitle = new TranslateTransition();
                 floatingTitle.setNode(island);
                 floatingTitle.setDelay(Duration.millis(Math.random()));
@@ -149,8 +148,46 @@ public class GameViewController extends InitialStage implements Controller {
         initializeClouds();
         reloadClouds();
         reloadIslands();
+        reloadBag();
     }
 
+    private void reloadBag() {
+        bag.setOnMouseClicked(mouseEvent -> {
+            //se la fase è giusta
+            //se sono il primo giocatore
+            //se è il mio turno
+            if (GUI.client.getLocalModel().getState().equals(State.PLANNINGPHASE) && GUI.client.getLocalModel().getFirstPlayer().toString().equals(GUI.client.getNickname()) && GUI.client.isMyTurn()) {
+                DrawFromBagCommand drawFromBagOrder = new DrawFromBagCommand(GUI.client.getNickname());
+                System.out.println("Drawing from bag...\n");
+
+                try {
+                    GUI.client.performGameAction(drawFromBagOrder);
+                } catch (NotEnoughCoinsException e) {
+                    Controller.showErrorDialogBox(StringNames.NOT_ENOUGH_COINS);
+                } catch (AssistantCardNotFoundException e) {
+                    Controller.showErrorDialogBox(StringNames.ASSISTANT_CARD_NOT_FOUND);
+                } catch (NegativeValueException e) {
+                    Controller.showErrorDialogBox(StringNames.NEGATIVE_VALUE);
+                } catch (IncorrectStateException e) {
+                    Controller.showErrorDialogBox(StringNames.INCORRECT_STATE);
+                } catch (MotherNatureLostException e) {
+                    Controller.showErrorDialogBox(StringNames.MOTHER_NATURE_LOST);
+                } catch (ProfessorNotFoundException e) {
+                    Controller.showErrorDialogBox(StringNames.PROFESSOR_NOT_FOUND);
+                } catch (IncorrectPlayerException e) {
+                    Controller.showErrorDialogBox(StringNames.INCORRECT_PLAYER);
+                } catch (RemoteException e) {
+                    Controller.showErrorDialogBox(StringNames.CONNECTION_ERROR);
+                } catch (IncorrectArgumentException e) {
+                    Controller.showErrorDialogBox(StringNames.INCORRECT_ARGUMENT);
+                } catch (UserNotInRoomException e) {
+                    Controller.showErrorDialogBox(StringNames.NOT_IN_ROOM);
+                } catch (UserNotRegisteredException e) {
+                    Controller.showErrorDialogBox(StringNames.USER_NOT_REGISTERED);
+                }
+            }
+        });
+    }
 
 
     public void reloadIslands() {
@@ -172,13 +209,12 @@ public class GameViewController extends InitialStage implements Controller {
 
 
         for (int i = 0; i < 6; i++) {
-            if(i==0||i==5){ //empty cell
-                Islands.addRow(0,new Text(""));
-            }
-            else{
+            if (i == 0 || i == 5) { //empty cell
+                Islands.addRow(0, new Text(""));
+            } else {
                 Pane island = new Pane();
                 islandsPanes.add(island);
-                ImageView islandImg = new ImageView(islandsImgs.get(i%3));
+                ImageView islandImg = new ImageView(islandsImgs.get(i % 3));
                 islandImg.setFitWidth(150);
                 islandImg.setFitHeight(150);
                 island.getChildren().add(islandImg);
@@ -187,13 +223,12 @@ public class GameViewController extends InitialStage implements Controller {
         }
 
         for (int i = 0; i < 6; i++) {
-            if(i!=0&&i!=5){ //empty cell
-                Islands.addRow(1,new Text(""));
-            }
-            else{
+            if (i != 0 && i != 5) { //empty cell
+                Islands.addRow(1, new Text(""));
+            } else {
                 Pane island = new Pane();
                 islandsPanes.add(island);
-                ImageView islandImg = new ImageView(islandsImgs.get(1+i%2));
+                ImageView islandImg = new ImageView(islandsImgs.get(1 + i % 2));
                 islandImg.setFitWidth(150);
                 islandImg.setFitHeight(150);
                 island.getChildren().add(islandImg);
@@ -202,13 +237,12 @@ public class GameViewController extends InitialStage implements Controller {
         }
 
         for (int i = 0; i < 6; i++) {
-            if(i!=0&&i!=5){ //empty cell
-                Islands.addRow(2,new Text(""));
-            }
-            else{
+            if (i != 0 && i != 5) { //empty cell
+                Islands.addRow(2, new Text(""));
+            } else {
                 Pane island = new Pane();
                 islandsPanes.add(island);
-                ImageView islandImg = new ImageView(islandsImgs.get(2-i%2));
+                ImageView islandImg = new ImageView(islandsImgs.get(2 - i % 2));
                 islandImg.setFitWidth(150);
                 islandImg.setFitHeight(150);
                 island.getChildren().add(islandImg);
@@ -217,13 +251,12 @@ public class GameViewController extends InitialStage implements Controller {
         }
 
         for (int i = 0; i < 6; i++) {
-            if(i==0||i==5){ //empty cell
-                Islands.addRow(3,new Text(""));
-            }
-            else{
+            if (i == 0 || i == 5) { //empty cell
+                Islands.addRow(3, new Text(""));
+            } else {
                 Pane island = new Pane();
                 islandsPanes.add(island);
-                ImageView islandImg = new ImageView(islandsImgs.get(i%3));
+                ImageView islandImg = new ImageView(islandsImgs.get(i % 3));
                 islandImg.setFitWidth(150);
                 islandImg.setFitHeight(150);
                 island.getChildren().add(islandImg);
@@ -578,5 +611,8 @@ public class GameViewController extends InitialStage implements Controller {
 
     @FXML
     private HBox IslandsBox;
+
+    @FXML
+    private StackPane bag;
 
 }
