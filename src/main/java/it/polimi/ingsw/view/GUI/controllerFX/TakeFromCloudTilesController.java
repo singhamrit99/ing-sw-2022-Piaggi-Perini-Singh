5c0e5a1995c62484c40f6f3ba7369f2b8bb2f38a
Amrit <amrit6422@gmail.com>
2022-06-14 22:08:25 +0200

    controller finished

--- src/main/java/it/polimi/ingsw/view/GUI/controllerFX/TakeFromCloudTilesController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/controllerFX/TakeFromCloudTilesController.java
@@ -1,15 +1,23 @@
 package it.polimi.ingsw.view.GUI.controllerFX;
 
+import it.polimi.ingsw.StringNames;
+import it.polimi.ingsw.exceptions.*;
 import it.polimi.ingsw.model.enumerations.Colors;
-import it.polimi.ingsw.model.tiles.Cloud;
+import it.polimi.ingsw.network.server.commands.PickCloud;
 import it.polimi.ingsw.network.server.stripped.StrippedCloud;
 import it.polimi.ingsw.view.GUI.GUI;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.text.Text;
 import javafx.stage.Window;
 import javafx.stage.WindowEvent;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TakeFromCloudTilesController extends InitialStage implements Controller {
     @FXML
@@ -19,6 +27,17 @@ public class TakeFromCloudTilesController extends InitialStage implements Contro
     @FXML
     private Button confirmButton;
 
+    @FXML
+    private Text totalYellow;
+    @FXML
+    private Text totalBlue;
+    @FXML
+    private Text totalGreen;
+    @FXML
+    private Text totalRed;
+    @FXML
+    private Text totalPink;
+
     public TakeFromCloudTilesController(GUI gui) {
         super(gui);
     }
@@ -26,6 +45,13 @@ public class TakeFromCloudTilesController extends InitialStage implements Contro
     @Override
     public void initialize() {
         boolean toAdd;
+        ArrayList<Text> text = new ArrayList<>();
+        text.add(totalYellow);
+        text.add(totalBlue);
+        text.add(totalGreen);
+        text.add(totalRed);
+        text.add(totalPink);
+
         for (int i = 0; i < GUI.client.getLocalModel().getClouds().size(); i++) {
 
             StrippedCloud cloud = GUI.client.getLocalModel().getClouds().get(i);
@@ -40,18 +66,22 @@ public class TakeFromCloudTilesController extends InitialStage implements Contro
                 cloudChoice.getItems().add(cloud.getName());
             }
         }
+        AtomicInteger selectedIndex = new AtomicInteger();
+        cloudChoice.setOnAction((event) -> {
+            selectedIndex.set(cloudChoice.getSelectionModel().getSelectedIndex());
+            EnumMap<Colors, Integer> students = GUI.client.getLocalModel().getClouds().get(selectedIndex.get()).getStudents();
 
-        //posso mostrare gli studenti che ci sono nella nuvola selezionata
-        //prendo studenti
-        //aggiungo immagini in base al colore e il X n corretto
+            for (int i = 0; i < students.size(); i++) {
+                text.get(i).setText(String.valueOf(students.get(Colors.getStudent(i))));
+            }
+        });
 
         //conferma
-        /*confirmButton.setOnAction((event) -> {
-            //prendo il numero da slider
+        confirmButton.setOnAction((event) -> {
+            PickCloud pickCloud = new PickCloud(GUI.client.getNickname(), selectedIndex.get());
 
-            MoveMotherNature moveMotherNature = new MoveMotherNature(GUI.client.getNickname(), (int) slider.getValue());
             try {
-                GUI.client.performGameAction(moveMotherNature);
+                GUI.client.performGameAction(pickCloud);
             } catch (NotEnoughCoinsException e) {
                 Controller.showErrorDialogBox(StringNames.NOT_ENOUGH_COINS);
             } catch (AssistantCardNotFoundException e) {
@@ -78,7 +108,7 @@ public class TakeFromCloudTilesController extends InitialStage implements Contro
 
             Window window = ((Node) (event.getSource())).getScene().getWindow();
             window.fireEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSE_REQUEST));
-        });*/
+        });
 
 
         cancelButton.setOnAction((event) -> {
