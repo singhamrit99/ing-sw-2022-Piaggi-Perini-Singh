1bcc63760d7df1f7c5515176a7fad7629f864312
Martino <tinopiaggi@gmail.com>
2022-06-14 08:34:57 +0200

    fix bug fill clouds

--- src/main/java/it/polimi/ingsw/view/GUI/controllerFX/GameViewController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/controllerFX/GameViewController.java
@@ -551,40 +551,44 @@ public class GameViewController extends InitialStage implements Controller {
             for (Colors c : students.keySet()) {
                 Image rightColor = studentImgFromColor(c);
                 if (students.get(c) != 0) {
-                    switch (cloudIndex) {
-                        case 0:
-                            if (numPlayers == 3) {
-                                studentsCloud1v3.get(indexStudentsAssets).setImage(rightColor);
-                                studentsCloud1v3.get(indexStudentsAssets).setVisible(true);
-                            } else {
-                                studentsCloud1v4.get(indexStudentsAssets).setImage(rightColor);
-                                studentsCloud1v4.get(indexStudentsAssets).setVisible(true);
-                            }
-                            break;
-                        case 1:
-                            if (numPlayers == 3) {
-                                studentsCloud2v3.get(indexStudentsAssets).setImage(rightColor);
-                                studentsCloud2v3.get(indexStudentsAssets).setVisible(true);
-                            } else {
-                                studentsCloud2v4.get(indexStudentsAssets).setImage(rightColor);
-                                studentsCloud2v4.get(indexStudentsAssets).setVisible(true);
-                            }
-                            break;
-                        case 2:
-                            if (numPlayers == 3) {
-                                studentsCloud3v3.get(indexStudentsAssets).setImage(rightColor);
-                                studentsCloud3v3.get(indexStudentsAssets).setVisible(true);
-                            } else {
-                                studentsCloud3v4.get(indexStudentsAssets).setImage(rightColor);
-                                studentsCloud3v4.get(indexStudentsAssets).setVisible(true);
-                            }
-                            break;
-                        case 3:
-                            studentsCloud4v4.get(indexStudentsAssets).setImage(rightColor);
-                            studentsCloud4v4.get(indexStudentsAssets).setVisible(true);
-                            break;
+                    int numSameColor = 0;
+                    while (numSameColor < students.get(c)) { //this in case of more students with same color
+                        switch (cloudIndex) {
+                            case 0:
+                                if (numPlayers == 3) {
+                                    studentsCloud1v3.get(indexStudentsAssets).setImage(rightColor);
+                                    studentsCloud1v3.get(indexStudentsAssets).setVisible(true);
+                                } else {
+                                    studentsCloud1v4.get(indexStudentsAssets).setImage(rightColor);
+                                    studentsCloud1v4.get(indexStudentsAssets).setVisible(true);
+                                }
+                                break;
+                            case 1:
+                                if (numPlayers == 3) {
+                                    studentsCloud2v3.get(indexStudentsAssets).setImage(rightColor);
+                                    studentsCloud2v3.get(indexStudentsAssets).setVisible(true);
+                                } else {
+                                    studentsCloud2v4.get(indexStudentsAssets).setImage(rightColor);
+                                    studentsCloud2v4.get(indexStudentsAssets).setVisible(true);
+                                }
+                                break;
+                            case 2:
+                                if (numPlayers == 3) {
+                                    studentsCloud3v3.get(indexStudentsAssets).setImage(rightColor);
+                                    studentsCloud3v3.get(indexStudentsAssets).setVisible(true);
+                                } else {
+                                    studentsCloud3v4.get(indexStudentsAssets).setImage(rightColor);
+                                    studentsCloud3v4.get(indexStudentsAssets).setVisible(true);
+                                }
+                                break;
+                            case 3:
+                                studentsCloud4v4.get(indexStudentsAssets).setImage(rightColor);
+                                studentsCloud4v4.get(indexStudentsAssets).setVisible(true);
+                                break;
+                        }
+                        numSameColor++;
+                        indexStudentsAssets++;
                     }
-                    indexStudentsAssets++;
                 }
             }
             //hides remaining students
@@ -868,8 +872,7 @@ public class GameViewController extends InitialStage implements Controller {
     // assets on screen
     @FXML
     private Menu changeViewBoard;
-    @FXML
-    private Menu currentViewPlayer;
+
 
     @FXML
     private MenuItem leaveGame;
