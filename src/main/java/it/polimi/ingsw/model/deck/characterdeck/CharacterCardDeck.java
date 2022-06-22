592dd9c394e4f45d685e31333849a27068ce9164
Sunriser <lorenzo.perini2000@gmail.com>
2022-06-22 21:53:00 +0200

    Javadocs

--- src/main/java/it/polimi/ingsw/model/deck/characterdeck/CharacterCardDeck.java
+++ src/main/java/it/polimi/ingsw/model/deck/characterdeck/CharacterCardDeck.java
@@ -20,6 +20,9 @@ public class CharacterCardDeck implements Deck<CharacterCard> {
         return characterCards;
     }
 
+    /**
+     * Fills Character cards deck with data from the Character Cards JSON file.
+     */
     @Override
     public void fillDeck() {
         Type collectionType = new TypeToken<ArrayList<CharacterCard>>() {
@@ -28,15 +31,28 @@ public class CharacterCardDeck implements Deck<CharacterCard> {
         characterCards = deckGenerator.getDeck();
     }
 
+    /**
+     * Returns the card's index.
+     * @param index A character card's unique identificator value.
+     * @return index int.
+     */
     public CharacterCard get(int index) {
         return characterCards.get(index);
     }
 
+    /**
+     * Returns the whole character deck.
+     * @return characterCards arraylist.
+     */
     @Override
     public ArrayList<CharacterCard> getAllCards() {
         return characterCards;
     }
 
+    /**
+     * Removes character card from the deck.
+     * @param index index of the card to remove.
+     */
     public void discardCard(int index) {
         characterCards.remove(index);
     }
