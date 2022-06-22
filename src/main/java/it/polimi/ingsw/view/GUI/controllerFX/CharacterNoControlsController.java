703a5ee2f3b92eabd33b1fe83986847bf68e972a
Amrit <amrit6422@gmail.com>
2022-06-22 17:53:14 +0200

    created class

---
+++ src/main/java/it/polimi/ingsw/view/GUI/controllerFX/CharacterNoControlsController.java
@@ -0,0 +1,74 @@
+package it.polimi.ingsw.view.GUI.controllerFX;
+
+import it.polimi.ingsw.StringNames;
+import it.polimi.ingsw.exceptions.*;
+import it.polimi.ingsw.network.server.commands.PlayCharacterCardA;
+import it.polimi.ingsw.network.server.stripped.StrippedCharacter;
+import it.polimi.ingsw.view.GUI.GUI;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
+
+import java.rmi.RemoteException;
+
+public class CharacterNoControlsController extends InitialStage implements Controller {
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private Text description;
+
+    public CharacterNoControlsController(GUI gui) {
+        super(gui);
+    }
+
+    @Override
+    public void initialize() {
+        StrippedCharacter selectedCharacter = GUI.client.getLocalModel().selectedCharacter;
+        description.setText(selectedCharacter.getDescription());
+
+        //conferma
+        confirmButton.setOnAction((event) -> {
+            PlayCharacterCardA playCharacterCardA = new PlayCharacterCardA(GUI.client.getNickname(), selectedCharacter.getCharacterID());
+            try {
+                GUI.client.performGameAction(playCharacterCardA);
+            } catch (NotEnoughCoinsException e) {
+                Controller.showErrorDialogBox(StringNames.NOT_ENOUGH_COINS);
+            } catch (AssistantCardNotFoundException e) {
+                Controller.showErrorDialogBox(StringNames.ASSISTANT_CARD_NOT_FOUND);
+            } catch (NegativeValueException e) {
+                Controller.showErrorDialogBox(StringNames.NEGATIVE_VALUE);
+            } catch (IncorrectStateException e) {
+                Controller.showErrorDialogBox(StringNames.INCORRECT_STATE);
+            } catch (MotherNatureLostException e) {
+                Controller.showErrorDialogBox(StringNames.MOTHER_NATURE_LOST);
+            } catch (ProfessorNotFoundException e) {
+                Controller.showErrorDialogBox(StringNames.PROFESSOR_NOT_FOUND);
+            } catch (IncorrectPlayerException e) {
+                Controller.showErrorDialogBox(StringNames.INCORRECT_PLAYER);
+            } catch (RemoteException e) {
+                Controller.showErrorDialogBox(StringNames.CONNECTION_ERROR);
+            } catch (IncorrectArgumentException e) {
+                Controller.showErrorDialogBox(StringNames.INCORRECT_ARGUMENT);
+            } catch (UserNotInRoomException e) {
+                Controller.showErrorDialogBox(StringNames.NOT_IN_ROOM);
+            } catch (UserNotRegisteredException e) {
+                Controller.showErrorDialogBox(StringNames.USER_NOT_REGISTERED);
+            }
+
+            Window window = ((Node) (event.getSource())).getScene().getWindow();
+            window.fireEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSE_REQUEST));
+        });
+
+        cancelButton.setOnAction((event) -> {
+            Window window = ((Node) (event.getSource())).getScene().getWindow();
+            window.fireEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSE_REQUEST));
+        });
+    }
+}
