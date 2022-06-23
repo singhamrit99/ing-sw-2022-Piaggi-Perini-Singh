d6b09ced4a40d9af585020bed28b0a5ae66dddb5
Sunriser <lorenzo.perini2000@gmail.com>
2022-06-24 00:32:42 +0200

    Javadocs

--- src/main/java/it/polimi/ingsw/model/StudentManager.java
+++ src/main/java/it/polimi/ingsw/model/StudentManager.java
@@ -6,6 +6,12 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public final class StudentManager {
+    /**
+     * StudentManager Method that adds students to a Students EnumMap.
+     * @param students The student EnumMap that will eventually be returned.
+     * @param studentsToAdd The students that need to be added.
+     * @return Updated Students EnumMap.
+     */
     public static EnumMap<Colors, Integer> addStudent(EnumMap<Colors, Integer> students, EnumMap<Colors, Integer> studentsToAdd) {
         for(Map.Entry<Colors, Integer> set : studentsToAdd.entrySet()) {
             if (set.getValue() >= 0) {
@@ -21,6 +27,12 @@ public final class StudentManager {
         return students;
     }
 
+    /**
+     * StudentManager method that removes students from the students EnumMap.
+     * @param students Initial students.
+     * @param studentsToRemove Students that need to be removed
+     * @return Updated Students EnumMap.
+     */
     public static EnumMap<Colors, Integer> removeStudent(EnumMap<Colors, Integer> students, EnumMap<Colors, Integer> studentsToRemove) {
         for (Map.Entry<Colors, Integer> set : studentsToRemove.entrySet()) {
             if (set.getValue() >= 0) {
@@ -34,6 +46,10 @@ public final class StudentManager {
         return students;
     }
 
+    /**
+     * Creates an empty Student EnumMap.
+     * @return The Empty Student EnumMap.
+     */
     public static EnumMap<Colors, Integer> createEmptyStudentsEnum() {
         EnumMap<Colors, Integer> students = new EnumMap<>(Colors.class);
         for (Colors color : Colors.values()) {
