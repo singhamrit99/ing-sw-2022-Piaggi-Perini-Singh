1a38ceb93c8df6aa8b3844ea7c5e584d6935a809
Sunriser <lorenzo.perini2000@gmail.com>
2022-06-24 10:58:34 +0200

    Javadocs

--- src/main/java/it/polimi/ingsw/network/server/stripped/StrippedBoard.java
+++ src/main/java/it/polimi/ingsw/network/server/stripped/StrippedBoard.java
@@ -21,6 +21,10 @@ public class StrippedBoard implements Serializable {
     private AssistantCardDeck deck;
     private int moves;
 
+    /**
+     * StrippedBoard class constructor method. Most of the parameters are setup in game based on the corresponding SchoolBoard.
+     *@param boardOwner The name of the player that this board corresponds to.
+     */
     public StrippedBoard(Player boardOwner) {
         this.owner = boardOwner.getNickname();
         SchoolBoard tmpBoard = boardOwner.getSchoolBoard();
@@ -37,66 +41,130 @@ public class StrippedBoard implements Serializable {
 
     }
 
+    /**
+     * Getter method for owner field.
+     * @return owner.
+     */
     public String getOwner() {
         return owner;
     }
 
+    /**
+     * Getter method for entrance enumMap.
+     * @return entrance.
+     */
     public EnumMap<Colors, Integer> getEntrance() {
         return entrance;
     }
 
+    /**
+     * Setter method for Entrance field.
+     * @param entrance the Student EnumMap the entrance has to be set to.
+     */
     public void setEntrance(EnumMap<Colors, Integer> entrance) {
         this.entrance = entrance;
     }
 
+    /**
+     * Getter method for Dining field.
+     * @return the dining room.
+     */
     public EnumMap<Colors, Integer> getDining() {
         return dining;
     }
 
+    /**
+     * Setter method for Dining field.
+     * @param dining the student enumMap the dining room has to be set to.
+     */
     public void setDining(EnumMap<Colors, Integer> dining) {
         this.dining = dining;
     }
 
+    /**
+     * Getter method for Coins field.
+     * @return coins (int)
+     */
     public int getCoins() {
         return coins;
     }
 
+    /**
+     * Setter method for Coins field.
+     * @param coins the player's new coins.
+     */
     public void setCoins(int coins) {
         this.coins = coins;
     }
 
+    /**
+     * Getter method for the professors table
+     * @return the professors ArrayList.
+     */
     public ArrayList<Colors> getProfessorsTable() {
         return professorsTable;
     }
 
+    /**
+     * Setter method for professors table field
+     * @param professorsTable the arrayList the professors table has to be set to.
+     */
     public void setProfessorsTable(ArrayList<Colors> professorsTable) {
         this.professorsTable = professorsTable;
     }
 
+    /**
+     * Getter method for numberOfTowers field
+     * @return numberOfTowers
+     */
     public int getNumberOfTowers() {
         return numberOfTowers;
     }
 
+    /**
+     * Setter method for numberOfTowers field
+     * @param numberOfTowers the updated number of towers.
+     */
     public void setNumberOfTowers(int numberOfTowers) {
         this.numberOfTowers = numberOfTowers;
     }
 
+    /**
+     * Getter method for this player's tower colors.
+     * @return the color of their towers.
+     */
     public Towers getColorsTowers(){
         return colorsTowers;
     }
 
+    /**
+     * Getter method for the Assistant Card deck.
+     * @return the Assistant Card deck.
+     */
     public AssistantCardDeck getDeck() {
         return deck;
     }
 
+    /**
+     * Setter method for the assistant card deck.
+     * @param deck Updated deck.
+     */
     public void setDeck(AssistantCardDeck deck) {
         this.deck = deck;
     }
 
+    /**
+     * Getter method for moves field (Mother Nature's moves)
+     * @return moves.
+     */
     public int getMoves() {
         return moves;
     }
 
+    /**
+     * Setter method for moves field
+     * @param moves updated moves.
+     */
     public void setMoves(int moves) {
         this.moves = moves;
     }
