0043507081ca4cd4a9e0873ca432805bbf8f5a8e
Sunriser <lorenzo.perini2000@gmail.com>
2022-06-24 11:23:58 +0200

    Javadocs

--- src/main/java/it/polimi/ingsw/network/server/stripped/StrippedCharacter.java
+++ src/main/java/it/polimi/ingsw/network/server/stripped/StrippedCharacter.java
@@ -18,6 +18,10 @@ public class StrippedCharacter implements Serializable {
     private int noEntryTiles;
     private EnumMap<Colors, Integer> students;
 
+    /**
+     * Constructor metohd for the Stripped Character class. Uses a full Character Card in Room.
+     * @param card the Character card this Stripped Character represents.
+     */
     public StrippedCharacter(CharacterCard card) {
         this.price = card.getPrice();
         this.description = card.getDescription();
@@ -31,30 +35,58 @@ public class StrippedCharacter implements Serializable {
         setNoEntryTiles(card.getNoTileNumber());
     }
 
+    /**
+     * Getter method for ID field.
+     * @return this Stripped Character's ID.
+     */
     public int getCharacterID() {
         return characterID;
     }
 
+    /**
+     * Setter method for the ID field. Only accessed at character creation.
+     * @param characterID the character ID of this card.
+     */
     public void setCharacterID(int characterID) {
         this.characterID = characterID;
     }
 
+    /**
+     * Getter method for price field.
+     * @return int price.
+     */
     public int getPrice() {
         return price;
     }
 
+    /**
+     * Setter method for price field.
+     * @param price the updated price of the card.
+     */
     public void setPrice(int price) {
         this.price = price;
     }
 
+    /**
+     * Getter method for the card's description.
+     * @return the card's description (string)
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Getter for the Type field.
+     * @return the card's Type.
+     */
     public Type getType() {
         return type;
     }
 
+    /**
+     * Getter for the Requirements field.
+     * @return the requirement needed for character activation.
+     */
     public Requirements getRequirements() {
         return requirements;
     }
@@ -65,18 +97,34 @@ public class StrippedCharacter implements Serializable {
                 && requirements.equals(other.getRequirements());
     }
 
+    /**
+     *Getter for the NoEntryTiles field.
+     * @return no entry tiles (int)
+     */
     public int getNoEntryTiles() {
         return noEntryTiles;
     }
 
+    /**
+     *Setter for the NoEntryTiles field.
+     * @param noEntryTiles the updated number of no Entry tiles on the card.
+     */
     public void setNoEntryTiles(int noEntryTiles) {
         this.noEntryTiles = noEntryTiles;
     }
 
+    /**
+     *Getter for the Students field.
+     * @return the students on the card (if any)
+     */
     public EnumMap<Colors, Integer> getStudents() {
         return students;
     }
 
+    /**
+     *Setter for the Students field.
+     * @param students the students the card needs to be set to.
+     */
     public void setStudents(EnumMap<Colors, Integer> students) {
         this.students = students;
     }
