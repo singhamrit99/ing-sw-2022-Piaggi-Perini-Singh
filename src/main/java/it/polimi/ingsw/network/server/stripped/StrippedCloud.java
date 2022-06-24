5f3f82012abd023a59fc3fd0b9584a20ff88d89a
Sunriser <lorenzo.perini2000@gmail.com>
2022-06-24 11:26:23 +0200

    Javadocs

--- src/main/java/it/polimi/ingsw/network/server/stripped/StrippedCloud.java
+++ src/main/java/it/polimi/ingsw/network/server/stripped/StrippedCloud.java
@@ -11,19 +11,35 @@ public class StrippedCloud implements Serializable {
     public String name;
     private EnumMap<Colors, Integer> students;
 
+    /**
+     * StrippedCloud constructor, used in room.
+     * @param cloud the full Cloud the StrippedCloud is built from.
+     */
     public StrippedCloud(Cloud cloud) {
         this.name = cloud.getName();
         this.students = cloud.getStudents();
     }
 
+    /**
+     * Getter for the Name field.
+     * @return the Stripped Cloud's name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter for the students field.
+     * @return the students EnumMap
+     */
     public EnumMap<Colors, Integer> getStudents() {
         return students;
     }
 
+    /**
+     * Setter for students field.
+     * @param students updated students.
+     */
     public void setStudents(EnumMap<Colors, Integer> students) {
         this.students = students;
     }
