4defb64415c85a781f0a16557a30c9a5e7f96392
Martino <tinopiaggi@gmail.com>
2022-06-25 18:03:01 +0200

    clientAppCli refactor

--- src/main/java/it/polimi/ingsw/ClientAppCLI.java
+++ src/main/java/it/polimi/ingsw/ClientAppCLI.java
@@ -3,13 +3,13 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.network.client.Client;
 import it.polimi.ingsw.view.CLI.CLI;
 import it.polimi.ingsw.exceptions.*;
+import it.polimi.ingsw.view.GUI.GUI;
 
 import java.io.IOException;
 
 public class ClientAppCLI {
     public static void main(String[] args) throws IOException, UserNotInRoomException, NotLeaderRoomException, NotEnoughCoinsException, AssistantCardNotFoundException, NegativeValueException, IncorrectStateException, MotherNatureLostException, ProfessorNotFoundException, IncorrectPlayerException, IncorrectArgumentException, UserNotRegisteredException, InterruptedException, RoomNotExistsException, LocalModelNotLoadedException, FullDiningException {
-        Client client = new Client("localhost",23023);
-        client.connect();
+        Client client = new Client("localhost", 23023);
         client.run();
         CLI cli = new CLI(client);
         cli.Start();
