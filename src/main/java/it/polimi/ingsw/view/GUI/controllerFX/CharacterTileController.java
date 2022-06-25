ae7be1e38d28c39fe8476f95f1b597f9f9d9faeb
Amrit <amrit6422@gmail.com>
2022-06-25 11:21:42 +0200

    added Exception

--- src/main/java/it/polimi/ingsw/view/GUI/controllerFX/CharacterTileController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/controllerFX/CharacterTileController.java
@@ -77,6 +77,8 @@ public class CharacterTileController extends InitialStage implements Controller 
                 Controller.showErrorDialogBox(StringNames.NOT_IN_ROOM);
             } catch (UserNotRegisteredException e) {
                 Controller.showErrorDialogBox(StringNames.USER_NOT_REGISTERED);
+            } catch (FullDiningException e) {
+                Controller.showErrorDialogBox(StringNames.DINING_WILL_FULL);
             }
 
             Window window = ((Node) (event.getSource())).getScene().getWindow();
