f5d1290c53f21ae06cb80345bce61a76d69d061e
Amrit <amrit6422@gmail.com>
2022-06-25 11:23:08 +0200

    added Exception

--- src/main/java/it/polimi/ingsw/network/server/commands/PlayCharacterCardC.java
+++ src/main/java/it/polimi/ingsw/network/server/commands/PlayCharacterCardC.java
@@ -1,10 +1,7 @@
 package it.polimi.ingsw.network.server.commands;
 
 import it.polimi.ingsw.controller.Controller;
-import it.polimi.ingsw.exceptions.IncorrectArgumentException;
-import it.polimi.ingsw.exceptions.NegativeValueException;
-import it.polimi.ingsw.exceptions.NotEnoughCoinsException;
-import it.polimi.ingsw.exceptions.ProfessorNotFoundException;
+import it.polimi.ingsw.exceptions.*;
 import it.polimi.ingsw.model.enumerations.Colors;
 
 import java.io.Serializable;
@@ -29,7 +26,7 @@ public class PlayCharacterCardC implements Command, Serializable {
         return playerCaller;
     }
     @Override
-    public void execute(Controller controller) throws NegativeValueException, IncorrectArgumentException, ProfessorNotFoundException, NotEnoughCoinsException {
+    public void execute(Controller controller) throws NegativeValueException, IncorrectArgumentException, ProfessorNotFoundException, NotEnoughCoinsException, FullDiningException {
         controller.callPlayCharacterCard(characterCardID, students1, students2);
     }
 }
