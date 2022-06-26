5a131423b2090b89334a5b15d897c71e5d057549
Sunriser <lorenzo.perini2000@gmail.com>
2022-06-26 11:05:10 +0200

    Javadocs

--- src/main/java/it/polimi/ingsw/ClientAppGUI.java
+++ src/main/java/it/polimi/ingsw/ClientAppGUI.java
@@ -6,6 +6,11 @@ import it.polimi.ingsw.view.GUI.GUI;
 import java.io.IOException;
 
 public class ClientAppGUI {
+    /**
+     * GUI app launcher.
+     * @param args jar startup arguments.
+     * @throws IOException Input exception.
+     */
     public static void main(String[] args) throws IOException {
         Client client = new Client("localhost", 23023);
         client.run();
--- src/main/java/it/polimi/ingsw/ServerApp.java
+++ src/main/java/it/polimi/ingsw/ServerApp.java
@@ -9,6 +9,12 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class ServerApp {
+    /**
+     * Starts the server application.
+     * @param args jar startup arguments.
+     * @throws RemoteException Thrown in case of a network error
+     * @throws AlreadyBoundException Thrown if the socket the server is connecting to is already bound.
+     */
     public static void main(String[] args) throws RemoteException, AlreadyBoundException {
             Server server = new Server();
             serverStub stub = server;
