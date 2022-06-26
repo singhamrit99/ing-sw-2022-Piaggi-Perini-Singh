1f06e41a86785872d67c1d685cb812169cd977f6
Amrit <amrit6422@gmail.com>
2022-06-26 15:06:37 +0200

    images fixed

--- src/main/java/it/polimi/ingsw/view/GUI/controllerFX/ResourcesPath.java
+++ src/main/java/it/polimi/ingsw/view/GUI/controllerFX/ResourcesPath.java
@@ -13,39 +13,39 @@ public interface ResourcesPath {
 
     // ----   GRAPHICS IMG IN GAME -----
     //towers
-    String BLACK_TOWER = "src/main/resources/img/towers/black_tower.png";
-    String GREY_TOWER = "src/main/resources/img/towers/grey_tower.png";
-    String WHITE_TOWER = "src/main/resources/img/towers/white_tower.png";
+    String BLACK_TOWER = "/img/towers/black_tower.png";
+    String GREY_TOWER = "/img/towers/grey_tower.png";
+    String WHITE_TOWER = "/img/towers/white_tower.png";
 
     //students
-    String BLUE_STUDENT = "src/main/resources/img/students/student_blue.png";
-    String GREEN_STUDENT = "src/main/resources/img/students/student_green.png";
-    String PINK_STUDENT = "src/main/resources/img/students/student_pink.png";
-    String RED_STUDENT = "src/main/resources/img/students/student_red.png";
-    String YELLOW_STUDENT = "src/main/resources/img/students/student_yellow.png";
+    String BLUE_STUDENT = "/img/students/student_blue.png";
+    String GREEN_STUDENT = "/img/students/student_green.png";
+    String PINK_STUDENT = "/img/students/student_pink.png";
+    String RED_STUDENT = "/img/students/student_red.png";
+    String YELLOW_STUDENT = "/img/students/student_yellow.png";
 
     //clouds only for 3players
-    String CLOUD_1= "src/main/resources/img/clouds/3players/cloud_3p_v1.png";
-    String CLOUD_2= "src/main/resources/img/clouds/3players/cloud_3p_v2.png";
-    String CLOUD_3= "src/main/resources/img/clouds/3players/cloud_3p_v3.png";
+    String CLOUD_1= "/img/clouds/3players/cloud_3p_v1.png";
+    String CLOUD_2= "/img/clouds/3players/cloud_3p_v2.png";
+    String CLOUD_3= "/img/clouds/3players/cloud_3p_v3.png";
 
     //islands
-    String ISLAND_0= "src/main/resources/img/islands/v0.png";
-    String ISLAND_1= "src/main/resources/img/islands/v1.png";
-    String ISLAND_2= "src/main/resources/img/islands/v2.png";
+    String ISLAND_0= "/img/islands/v0.png";
+    String ISLAND_1= "/img/islands/v1.png";
+    String ISLAND_2= "/img/islands/v2.png";
 
     //MotherNature
-    String MN = "src/main/resources/img/islands/mn.png";
+    String MN = "/img/islands/mn.png";
 
     //Assistants
-    String ASSISTANT_CARDS = "src/main/resources/img/assistants/";
+    String ASSISTANT_CARDS = "/img/assistants/";
     String IMAGE_EXTENSION_ASS = ".png";
 
     //css
-    String TEST_CSS = "src/main/resources/css/MainMenuStyle.css";
+    String TEST_CSS = "/css/MainMenuStyle.css";
 
     //Characters
-    String CHARACTERS = "src/main/resources/img/characters/character";
+    String CHARACTERS = "/img/characters/character";
     String IMAGE_EXTENSION_CHAR = ".jpg";
-    String NOENTRYTILE = "src/main/resources/img/characters/effects/noEntry.png";
+    String NOENTRYTILE = "/img/characters/effects/noEntry.png";
 }
