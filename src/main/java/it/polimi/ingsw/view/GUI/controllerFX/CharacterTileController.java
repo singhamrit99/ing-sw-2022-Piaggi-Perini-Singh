6310dbae6133358c6ceeefcdfd263acb9fd9ad14
Martino <tinopiaggi@gmail.com>
2022-06-27 07:53:49 +0200

    Refactor according to stringNames

--- src/main/java/it/polimi/ingsw/view/GUI/controllerFX/CharacterTileController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/controllerFX/CharacterTileController.java
@@ -86,7 +86,7 @@ public class CharacterTileController extends InitialStage implements Controller 
             } catch (UserNotRegisteredException e) {
                 Controller.showErrorDialogBox(StringNames.USER_NOT_REGISTERED);
             } catch (FullDiningException e) {
-                Controller.showErrorDialogBox(StringNames.DINING_WILL_FULL);
+                Controller.showErrorDialogBox(StringNames.FULL_DINING);
             } catch (CardPlayedInTurnException e) {
                 Controller.showErrorDialogBox(StringNames.CARD_PLAYED_IN_TURN);
             }
