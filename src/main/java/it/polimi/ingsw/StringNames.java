562492027210da40bef32073bab7c4bdc5e01dfd
Amrit <amrit6422@gmail.com>
2022-06-27 10:32:34 +0200

    new exception added

--- src/main/java/it/polimi/ingsw/StringNames.java
+++ src/main/java/it/polimi/ingsw/StringNames.java
@@ -19,55 +19,32 @@ public interface StringNames {
     String CREATE_NEW_ROOM = "CreateRoom";
 
     //used for alerts
-
     String ASSISTANT_CARD_NOT_FOUND = "Card not found";
-
-    String CARD_PLAYED_IN_TURN = "This card is already played by another player";
-
-
+    String CARD_PLAYED_IN_TURN = "You have already played a card";
     String FULL_DINING = "You are adding more than you can put";
     String INCORRECT_ARGUMENT = "Given parameters are not correct";
-
     String INCORRECT_PLAYER = "Incorrect player";
-
     String INCORRECT_STATE = "You are not in the correct state in the game";
-
     String LOCAL_MODEL_ERROR = "Critical bug with local model";
-
     String MOTHER_NATURE_LOST = "Bug while placing Mother Nature";
-
     String NAME_FIELD_NULL = "Name field is not filled or blank";
-
     String NEGATIVE_VALUE = "Negative value";
-
     String NOT_ENOUGH_COINS = "Not enough coins to play card";
-
     String NOT_ENOUGH_PLAYERS = "You are alone in this room. There must be at least 2 players to play Eryantis";
-
     String NOT_LEADER = "You don't have permission to perform this action";
-
     String PROFESSOR_NOT_FOUND = "Professor not found";
-
     String ROOM_ALREADY_EXISTS = "Room name already taken";
-
     String ROOM_FULL = "The room is full 4/4";
-
     String ROOM_IN_GAME = "This room is playing at the moment";
-
     String ROOM_NOT_EXISTS = "There is no such room";
-
     String USER_ALREADY_EXISTS = "Nickname is already chosen by someone else";
-
     String USER_IN_ROOM = "You are already in the room";
-
     String USER_NOT_IN_ROOM = "You are not in the room";
-
     String USER_NOT_REGISTERED = "User is not registered in the server";
-
-    String CONNECTION_ERROR = "There is an issue with the connection";
-
+    String REMOTE = "There is an issue with the connection";
     String ERROR_IO = "Error while loading image assets";
-
     String INTERRUPTED = "Client error while waiting updates";
-
+    String ASSISTANT_CARD_ALREADY_PLAYED = "Card has been played by another player";
+    String RUNTIME = "Runtime error";
+    String NUMBER_FORMAT = "That's not a number! Try again.\n";
 }
